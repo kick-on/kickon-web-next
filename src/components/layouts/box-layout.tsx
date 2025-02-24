b8525742878124a0c8460c6fc308a010bc4062ef
지민재 <jim400934@gmail.com>
2025-02-24 16:23:56 +0900

    refact: 좌우 패딩 삭제

--- src/components/layouts/box-layout.tsx
+++ src/components/layouts/box-layout.tsx
@@ -6,15 +6,15 @@ import clsx from 'clsx';
 const commonPaddingPages = new Set(['/page2', '/page3']);
 
 const paddingMap: Record<string, string> = {
-	'/signup': 'px-[10.125rem] py-[6.25rem]',
-	'/page4': 'pt-[7.5rem] pb-14 px-[10.125rem]', // 프로필 설정 페이지
+	'/signup': 'py-[6.25rem]',
+	'/page4': 'pt-[7.5rem] pb-14', // 프로필 설정 페이지
 };
 
 const WhiteBox: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const pathname = usePathname();
 
 	const computedPadding = commonPaddingPages.has(pathname)
-		? 'pt-[1.875rem] pb-[6.25rem] px-4'
+		? 'pt-[1.875rem] pb-[6.25rem]'
 		: paddingMap[pathname] || 'p-5';
 
 	return (
