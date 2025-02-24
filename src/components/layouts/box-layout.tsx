9870173c3106fb71ae78e629d4cbac9f13c587db
지민재 <jim400934@gmail.com>
2025-02-24 15:50:29 +0900

    refact: 피드백 반영

--- src/components/layouts/box-layout.tsx
+++ src/components/layouts/box-layout.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// 패딩이 동일한 페이지들을 조건부로 처리
 const commonPaddingPages = new Set(['/page2', '/page3']);
 
 const paddingMap: Record<string, string> = {
@@ -14,11 +13,15 @@ const paddingMap: Record<string, string> = {
 const WhiteBox: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const pathname = usePathname();
 
+	const computedPadding = commonPaddingPages.has(pathname)
+		? 'pt-[1.875rem] pb-[6.25rem] px-4'
+		: paddingMap[pathname] || 'p-5';
+
 	return (
 		<div
 			className={clsx(
-				'bg-black-100 w-fit min-h-fit border border-black-300 border-solid rounded-[0.625rem] mt-4 m-auto',
-				commonPaddingPages.has(pathname) ? 'pt-[1.875rem] pb-[6.25rem] px-4' : paddingMap[pathname] || 'p-5', // 조건부 패딩 적용
+				'bg-black-100 w-[41.75rem] h-auto border border-black-300 border-solid rounded-[0.625rem] mt-4 m-auto',
+				computedPadding,
 			)}
 		>
 			{children}
