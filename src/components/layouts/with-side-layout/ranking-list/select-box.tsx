109ef4ae092d9e92395f9ef67a17f31e25a982bb
young <azerosong@gmail.com>
2025-03-01 16:02:12 +0900

    feat: 리그를 상수로 분리

--- src/components/layouts/with-side-layout/ranking-list/select-box.tsx
+++ src/components/layouts/with-side-layout/ranking-list/select-box.tsx
@@ -4,42 +4,12 @@ import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import OptionItem from './option-item';
 import clsx from 'clsx';
+import { leagues } from '@/lib/constants/leagues';
 
 export default function SelectBox() {
 	const [isVisibleOptions, setIsVisibleOptions] = useState(false);
 	const [league, setLeague] = useState('K리그 1');
 	const dropboxRef = useRef<HTMLDivElement | null>(null);
-
-	const leagues = [
-		{
-			league: '프리미어 리그',
-			src: '/league-logo/premier-league.svg',
-		},
-		{
-			league: '라리가',
-			src: '/league-logo/la-liga.svg',
-		},
-		{
-			league: '분데스리가',
-			src: '/league-logo/bundesliga.svg',
-		},
-		{
-			league: '세리에 A',
-			src: '/league-logo/serie-a.svg',
-		},
-		{
-			league: '리그앙',
-			src: '/league-logo/ligue-1.svg',
-		},
-		{
-			league: 'K리그 1',
-			src: '/league-logo/k-league.svg',
-		},
-		{
-			league: 'K리그 2',
-			src: '/league-logo/k-league.svg',
-		},
-	];
 
 	const handleSelectBoxClick = () => {
 		setIsVisibleOptions(!isVisibleOptions);
