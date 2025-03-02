7986ea27ad7968b5fbe5d05893fe868ae3dcf360
young <azerosong@gmail.com>
2025-03-02 20:32:09 +0900

    style: ComponentFrame에 border가 없는 옵션 추가

--- src/components/common/componentFrame.tsx
+++ src/components/common/componentFrame.tsx
@@ -1,6 +1,19 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
-export default function ComponentFrame({ isMain = false, children }: { isMain?: boolean; children: ReactNode }) {
+export default function ComponentFrame({
+	isMain = false,
+	hasBorder = true,
+	children,
+}: {
+	isMain?: boolean;
+	hasBorder?: boolean;
+	children: ReactNode;
+}) {
 	const width = isMain ? 'w-[678px]' : 'w-[322px]';
-	return <div className={`flex flex-col ${width} bg-black-000 border border-black-300 rounded-[10px]`}>{children}</div>;
+	return (
+		<div className={clsx('flex flex-col bg-black-000 rounded-[10px]', width, { 'border border-black-300': hasBorder })}>
+			{children}
+		</div>
+	);
 }
