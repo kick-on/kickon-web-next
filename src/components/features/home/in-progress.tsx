8413d52ea7885fc44f99684ffb1002f11ab14ca8
young <azerosong@gmail.com>
2025-03-02 20:54:09 +0900

    refactor: in-progress 버튼 클릭 함수 동작에 관련하여 주석 추가

--- src/components/features/home/in-progress.tsx
+++ src/components/features/home/in-progress.tsx
@@ -42,26 +42,33 @@ export default function InProgress() {
 
 	const handleTeamButtonClick = (button: SelectedButton) => {
 		if (isCompleted) {
+			// 선택이 완료된 상태에서 다시 클릭 시 득점 업다운 버튼 활성화
 			setIsCompleted(false);
 			setIsClicked(true);
 		} else {
 			if (selectedButton === button) {
+				// 동일 버튼 클릭 시 선택 종료
 				setIsClicked(false);
 				setLeftScore(0);
 				setRightScore(0);
 			} else {
 				if (button === 'center') {
-					const maxScore = Math.max(leftScore, rightScore);
-					if (leftScore !== maxScore) {
-						setLeftScore(maxScore);
-					} else if (rightScore !== maxScore) {
-						setRightScore(maxScore);
+					// 무승부 버튼 클릭 시 더 큰 값에 동기화
+					if (leftScore > rightScore) {
+						setRightScore(leftScore);
+					} else {
+						setLeftScore(rightScore);
 					}
-				} else if (selectedButton === 'left' && button === 'right') {
+				} else if (button === 'right') {
+					// leftScore > rightScore 상태에서 오른쪽 팀 버튼 클릭 시
+					// rightScore = leftScore + 1
 					setRightScore(leftScore + 1);
-				} else if (selectedButton === 'right' && button === 'left') {
+				} else if (button === 'left') {
+					// rightScore > leftScore 상태에서 왼쪽 팀 버튼 클릭 시
+					// leftScore = rightScore + 1
 					setLeftScore(rightScore + 1);
 				} else {
+					// 그 외에는 한쪽을 1 증가하거나 양쪽을 1 증가
 					increaseScore[button]();
 				}
 				setIsClicked(true);
@@ -71,6 +78,7 @@ export default function InProgress() {
 
 	const handleUpButtonClick = (side: 'left' | 'right') => {
 		if (selectedButton === 'center') {
+			// 무승부 상태에서 버튼 클릭 시 양쪽을 1 증가
 			increaseScore['center']();
 		} else {
 			increaseScore[side]();
@@ -79,6 +87,7 @@ export default function InProgress() {
 
 	const handleDownButtonClick = (side: 'left' | 'right') => {
 		if (selectedButton === 'center') {
+			// 무승부 상태에서 버튼 클릭 시 양쪽을 1 감소
 			decreaseScore['center']();
 		} else {
 			decreaseScore[side]();
@@ -92,7 +101,9 @@ export default function InProgress() {
 
 	return (
 		<div className="flex flex-col gap-2.5 w-[36rem]">
+			{/* 팀 선택 버튼 */}
 			<div className="relative w-full h-[4.625rem] grid grid-cols-3 border border-black-200 rounded-[0.625rem] button3-semibold shadow-predict-button">
+				{/* 선택한 팀 표시 */}
 				{(isClicked || isCompleted) && (
 					<div className="absolute pointer-events-none top-1/2 left-1/2 -translate-1/2 z-10 w-full h-[4.625rem] grid grid-cols-[1fr_11.9063rem_1fr]">
 						<div
@@ -112,6 +123,8 @@ export default function InProgress() {
 						></div>
 					</div>
 				)}
+
+				{/* 왼쪽 팀 */}
 				<div
 					onClick={() => handleTeamButtonClick('left')}
 					className={clsx('pl-4 h-full flex gap-2 items-center rounded-l-[0.5625rem]')}
@@ -122,6 +135,8 @@ export default function InProgress() {
 						{isClicked && <div className="relative z-20 caption2-medium text-black-800">53%</div>}
 					</div>
 				</div>
+
+				{/* 중앙 (무승부) */}
 				<div
 					onClick={() => handleTeamButtonClick('center')}
 					className={clsx('relative h-full flex flex-col justify-center items-center border-x border-black-200')}
@@ -149,6 +164,8 @@ export default function InProgress() {
 					<div className="relative z-20">무승부</div>
 					{isClicked && <div className="relative z-20 caption2-medium text-black-800">5%</div>}
 				</div>
+
+				{/* 오른쪽 팀 */}
 				<div
 					onClick={() => handleTeamButtonClick('right')}
 					className={clsx('pr-4 h-full flex gap-2 justify-end items-center text-right rounded-r-[0.5625rem]')}
@@ -160,6 +177,8 @@ export default function InProgress() {
 					<Image className="relative z-20" width={22} height={22} src="/team-logo/ulsan.svg" alt="FC 서울" />
 				</div>
 			</div>
+
+			{/* 선택 완료 버튼 */}
 			{isClicked && (
 				<button
 					onClick={handleCompleteButtonClick}
