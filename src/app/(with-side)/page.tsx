d6cf58195b5e707ff035b4a249dd6b57ac0fab98
young <azerosong@gmail.com>
2025-03-02 23:10:00 +0900

    feat: 마감된 승부예측 카드 UI 구현

--- src/app/(with-side)/page.tsx
+++ src/app/(with-side)/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import ComponentFrame from '@/components/common/componentFrame';
-import InProgress from '@/components/features/home/in-progress';
+import PredictCard from '@/components/features/home/predict-card';
 import { useEffect } from 'react';
 
 export default function Home() {
@@ -14,8 +13,15 @@ export default function Home() {
 	}, []);
 
 	return (
-		<ComponentFrame isMain={true} hasBorder={false}>
-			<InProgress />
-		</ComponentFrame>
+		<div className="flex flex-col gap-8">
+			<div className="flex flex-col gap-4">
+				<PredictCard status="inProgress" />
+			</div>
+			<hr className="mx-6 border-black-600" />
+			<div className="flex flex-col gap-4">
+				<PredictCard status="participated" />
+				<PredictCard status="unParticipated" />
+			</div>
+		</div>
 	);
 }
