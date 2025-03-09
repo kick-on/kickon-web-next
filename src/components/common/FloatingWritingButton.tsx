f709d77323ed35162edfb932e7917842c77b2746
지민재 <jim400934@gmail.com>
2025-03-09 13:22:52 +0900

    refactor: edit 아이콘 하나의 div 태그 안에서 관리하며 깜빡거리는 효과 삭제

--- src/components/common/FloatingWritingButton.tsx
+++ src/components/common/FloatingWritingButton.tsx
@@ -7,18 +7,14 @@ const FloatingWritingButton = () => {
 	return (
 		<button
 			className="z-50 flex items-center h-12 w-12 fixed top-214 left-[calc(100vw-37rem)]
-                bg-black-700 text-white rounded-full shadow-lg overflow-hidden group
-                transition-all duration-300 ease-in-out hover:w-[322px] hover:rounded-[50px] hover:justify-center"
+              bg-black-700 text-white rounded-full shadow-lg overflow-hidden group
+              transition-all duration-300 ease-in-out hover:w-[322px] hover:pl-[4.75rem]"
 		>
-			{/* 아이콘만 보이는 기본 상태 */}
-			<div className="absolute inset-0 flex items-center justify-center transition-opacity duration-200 group-hover:opacity-0">
+			<div className="flex items-center gap-2 pl-3 w-full">
 				<Image src={Icon} alt="아이콘" className="w-6 h-6 min-w-6" />
-			</div>
-
-			{/* 호버 시 나타나는 텍스트와 아이콘 */}
-			<div className="flex items-center gap-2 px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200 w-full justify-center">
-				<Image src={Icon} alt="아이콘" className="w-6 h-6 min-w-6" />
-				<span className="whitespace-nowrap">새로운 글 작성하기</span>
+				<span className="whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
+					새로운 글 작성하기
+				</span>
 			</div>
 		</button>
 	);
