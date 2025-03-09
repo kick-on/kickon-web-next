edece7281657159263f7241ac88895aa99fe7966
지민재 <jim400934@gmail.com>
2025-03-09 13:39:47 +0900

    refact: 아이콘 m-width제거 후 next.js Image의 width, height props 활용

--- src/components/common/FloatingWritingButton.tsx
+++ src/components/common/FloatingWritingButton.tsx
@@ -6,12 +6,12 @@ import Icon from '../../../public/edit.svg';
 const FloatingWritingButton = () => {
 	return (
 		<button
-			className="z-50 flex items-center h-12 w-12 fixed top-214 left-[calc(100vw-37rem)]
+			className="z-50 flex items-center w-[3.625rem] h-[3.625rem] fixed top-214 left-[calc(100vw-37rem)]
               bg-black-700 text-white rounded-full shadow-lg overflow-hidden group
               transition-all duration-300 ease-in-out hover:w-[322px] hover:pl-[4.75rem]"
 		>
 			<div className="flex items-center gap-2 pl-3 w-full">
-				<Image src={Icon} alt="아이콘" className="w-6 h-6 min-w-6" />
+				<Image src={Icon} alt="아이콘" width={24} height={24} />
 				<span className="whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
 					새로운 글 작성하기
 				</span>
