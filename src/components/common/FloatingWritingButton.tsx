5b2d53c37b9160936377dc515a45cf7ad8893338
지민재 <jim400934@gmail.com>
2025-03-09 15:59:42 +0900

    refact: with-side 레이아웃에 플로팅 버튼 컴포넌트 추가, home 페이지 제외 렌더링

--- src/components/common/FloatingWritingButton.tsx
+++ src/components/common/FloatingWritingButton.tsx
@@ -6,13 +6,14 @@ import Icon from '../../../public/edit.svg';
 const FloatingWritingButton = () => {
 	return (
 		<button
-			className="z-50 flex items-center w-[3.625rem] h-[3.625rem] fixed top-214 left-[calc(100vw-37rem)]
-              bg-black-700 text-white rounded-full shadow-lg overflow-hidden group
-              transition-all duration-300 ease-in-out hover:w-[322px] hover:pl-[4.75rem]"
+			className="z-50 flex items-center w-[3.625rem] h-[3.625rem] fixed 
+					  bottom-5 bg-black-700 text-white rounded-full shadow-lg 
+					  overflow-hidden group transition-all duration-300 ease-in-out 
+					  hover:w-[20.125rem] hover:pl-[3.75rem]"
 		>
-			<div className="flex items-center gap-2 pl-3 w-full">
-				<Image src={Icon} alt="아이콘" width={24} height={24} />
-				<span className="whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
+			<div className="flex items-center gap-2 px-4 w-full">
+				<Image src={Icon} alt="아이콘" width={28} height={28} />
+				<span className="button2-semibold whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
 					새로운 글 작성하기
 				</span>
 			</div>
