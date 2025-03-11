b605c42ec6913869b4ed33c7242a3350bc5f0a8c
young <azerosong@gmail.com>
2025-03-11 13:59:53 +0900

    feat: 로그인 모달 UI 구현

---
+++ src/components/common/login-modal/login-modal.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import Image from 'next/image';
+import LoginButton from './login-button';
+import { useEffect, useRef } from 'react';
+
+export default function LoginModal({ onClose }) {
+	const modalRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		const handleOutsideClick = (e: MouseEvent) => {
+			if (modalRef && !modalRef.current.contains(e.target as Node)) {
+				onClose();
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, [onClose]);
+
+	return (
+		<div className="fixed top-0 left-0 w-full h-full bg-black/40">
+			<div
+				ref={modalRef}
+				className="flex flex-col items-center mt-[11.875rem] mx-auto p-6
+            w-[39.5rem] h-[39.75rem] bg-black-000 rounded-[0.625rem] display-semibold"
+			>
+				<Image
+					onClick={onClose}
+					width={24}
+					height={24}
+					src="/x.svg"
+					alt="닫기"
+					className="ml-auto mb-[4.6875rem] cursor-pointer"
+				/>
+				<Image width={280} height={62} src="/logo/kick-on-mixed.svg" alt="킥온 로고" />
+
+				<div className="mt-[1.875rem]">로그인하고 킥온과 함께</div>
+				<div>좋아하는 축구팀 승부예측하러 가요!</div>
+
+				<div className="mt-[6.625rem] flex flex-col gap-5">
+					<LoginButton social="카카오" onClick={() => {}} />
+					<LoginButton social="네이버" onClick={() => {}} />
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/components/layouts/with-side-layout/profile.tsx
+++ src/components/layouts/with-side-layout/profile.tsx
@@ -1,62 +1,79 @@
 'use client';
 
 import ComponentFrame from '@/components/common/componentFrame';
+import LoginModal from '@/components/common/login-modal/login-modal';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Profile() {
 	const [isLoggedin, setIsLoggedin] = useState(false);
+	const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+	const handleLoginButtonClick = () => {
+		setIsLoginModalOpen(true);
+	};
+
+	const handleLoginModalClose = () => {
+		setIsLoginModalOpen(false);
+	};
+
 	return (
-		<ComponentFrame>
-			{isLoggedin ? (
-				<div>
-					<div className="flex p-4 justify-between border-b border-black-200">
-						<div className="flex gap-3">
-							<Image width={60} height={60} src="/default-profile.svg" alt="프로필 사진" />
-							<div className="flex flex-col gap-[0.3125rem] mt-[0.4688rem]">
-								<div className="flex gap-2">
-									<div className="flex gap-1">
-										<div className="body2-semibold">닉네임</div>
-										<div className="body2-regular text-black-800">님</div>
+		<>
+			{isLoginModalOpen && <LoginModal onClose={handleLoginModalClose} />}
+			<ComponentFrame>
+				{isLoggedin ? (
+					<div>
+						<div className="flex p-4 justify-between border-b border-black-200">
+							<div className="flex gap-3">
+								<Image width={60} height={60} src="/default-profile.svg" alt="프로필 사진" />
+								<div className="flex flex-col gap-[0.3125rem] mt-[0.4688rem]">
+									<div className="flex gap-2">
+										<div className="flex gap-1">
+											<div className="body2-semibold">닉네임</div>
+											<div className="body2-regular text-black-800">님</div>
+										</div>
+										<Image width={16} height={16} src="/team-logo/ulsan.svg" alt="팀 로고" />
 									</div>
-									<Image width={16} height={16} src="/team-logo/ulsan.svg" alt="팀 로고" />
+									<Link href="/setting" className="flex gap-0.5 button6-regular text-black-700 underline">
+										프로필 설정
+										<Image width={10} height={10} src="/chevron/right-gray.svg" alt="바로가기" />
+									</Link>
 								</div>
-								<Link href="/setting" className="flex gap-0.5 button6-regular text-black-700 underline">
-									프로필 설정
-									<Image width={10} height={10} src="/chevron/right-gray.svg" alt="바로가기" />
-								</Link>
 							</div>
+							<button className="mr-2.5 mt-0 h-fit button6-regular text-black-700 underline">로그아웃</button>
 						</div>
-						<button className="mr-2.5 mt-0 h-fit button6-regular text-black-700 underline">로그아웃</button>
-					</div>
-					<div className="grid grid-cols-2">
-						<div className="flex border-r border-black-200">
-							<div className="mx-auto my-[0.5625rem] text-center">
-								<div className="caption2-regular h-4">이번 시즌 팀 내 순위</div>
-								<div className="body4-semibold">1위</div>
-							</div>
-						</div>
-						<div className="flex">
-							<div className="mx-auto my-[0.5625rem] text-center">
-								<div className="flex gap-1 caption2-regular h-4">
-									지금까지 모은 포인트
-									<Image width={12} height={12} src="/help-circle.svg" alt="도움말" />
+						<div className="grid grid-cols-2">
+							<div className="flex border-r border-black-200">
+								<div className="mx-auto my-[0.5625rem] text-center">
+									<div className="caption2-regular h-4">이번 시즌 팀 내 순위</div>
+									<div className="body4-semibold">1위</div>
 								</div>
-								<div className="body4-semibold">512 P</div>
+							</div>
+							<div className="flex">
+								<div className="mx-auto my-[0.5625rem] text-center">
+									<div className="flex gap-1 caption2-regular h-4">
+										지금까지 모은 포인트
+										<Image width={12} height={12} src="/help-circle.svg" alt="도움말" />
+									</div>
+									<div className="body4-semibold">512 P</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			) : (
-				<div className="mx-auto my-[1.8125rem] flex flex-col items-center gap-4">
-					<Image width={40} height={32} src="/logo/icon-black.svg" alt="킥온" />
-					<button className="flex gap-2.5 items-center w-fit px-[1.125rem] py-2.5 bg-primary-900 rounded-3xl shadow-login-button">
-						<div className="text-black-000 button1-medium">간편 로그인 하기</div>
-						<Image width={18} height={18} src="/chevron/right-white.svg" alt="바로가기" />
-					</button>
-				</div>
-			)}
-		</ComponentFrame>
+				) : (
+					<div className="mx-auto my-[1.8125rem] flex flex-col items-center gap-4">
+						<Image width={40} height={32} src="/logo/icon-black.svg" alt="킥온" />
+						<button
+							onClick={handleLoginButtonClick}
+							className="flex gap-2.5 items-center w-fit px-[1.125rem] py-2.5 bg-primary-900 rounded-3xl shadow-login-button"
+						>
+							<div className="text-black-000 button1-medium">간편 로그인 하기</div>
+							<Image width={18} height={18} src="/chevron/right-white.svg" alt="바로가기" />
+						</button>
+					</div>
+				)}
+			</ComponentFrame>
+		</>
 	);
 }
