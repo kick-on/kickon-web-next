5035fc67d947cd06cf9bc6db6c5e81b720de4d81
지민재 <jim400934@gmail.com>
2025-03-12 11:21:58 +0900

    feat: 게시글 작성 페이지 레이아웃 설정

---
+++ src/app/(without-side)/post/news/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+	return <>뉴스 작성 페이지</>;
+}
--- src/components/layouts/without-side/white-box.tsx
+++ src/components/layouts/without-side/white-box.tsx
@@ -7,6 +7,8 @@ const commonPaddingPages = new Set(['/page2', '/page3']);
 
 const paddingMap: Record<string, string> = {
 	'/signup': 'py-[6.25rem]',
+	'/post/news': 'px-4 pt-7.5',
+	'/post/board': 'px-4 pt-7.5',
 	'/page4': 'pt-[7.5rem] pb-14', // 프로필 설정 페이지
 };
 
@@ -20,7 +22,7 @@ const WhiteBox: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	return (
 		<div
 			className={clsx(
-				'bg-black-100 w-[41.75rem] h-auto border border-black-300 border-solid rounded-[0.625rem] mt-4 m-auto',
+				'flex bg-black-100 w-[41.75rem] h-auto border border-black-300 border-solid rounded-[0.625rem] mt-4 m-auto',
 				computedPadding,
 			)}
 		>
