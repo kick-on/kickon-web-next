ea2a4becf49dc06132abc975c28a52944332279e
young <azerosong@gmail.com>
2025-03-13 16:52:30 +0900

    rename: with-side 레이아웃의 profile -> account로 변경

--- src/components/layouts/with-side-layout/account.tsx
+++ src/components/layouts/with-side-layout/account.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
-export default function Profile() {
+export default function Account() {
 	const [isLoggedin, setIsLoggedin] = useState(false);
 	return (
 		<ComponentFrame>
