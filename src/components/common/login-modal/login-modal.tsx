b17875bbcd69c568224ccb009cfa807d8fa7e621
young <azerosong@gmail.com>
2025-03-17 15:44:10 +0900

    refactor: 로그인 모달 x 버튼을 button 태그로 래핑

--- src/components/common/login-modal/login-modal.tsx
+++ src/components/common/login-modal/login-modal.tsx
@@ -29,14 +29,9 @@ export default function LoginModal({ onClose }) {
 				className="flex flex-col items-center p-6 shadow-predict-button
             w-[39.5rem] h-[39.75rem] bg-black-000 rounded-[0.625rem] display-semibold"
 			>
-				<Image
-					onClick={onClose}
-					width={24}
-					height={24}
-					src="/x.svg"
-					alt="닫기"
-					className="ml-auto mb-[4.6875rem] cursor-pointer"
-				/>
+				<button onClick={onClose} className="ml-auto mb-[4.6875rem]">
+					<Image width={24} height={24} src="/x.svg" alt="닫기" />
+				</button>
 				<Image width={280} height={62} src="/logo/kick-on-mixed.svg" alt="킥온 로고" />
 
 				<div className="mt-[1.875rem]">로그인하고 킥온과 함께</div>
