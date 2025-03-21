76ad886d9676752510d5aebfd1d8fa75c9156e62
young <azerosong@gmail.com>
2025-03-21 13:39:56 +0900

    faet: 랭킹 관련 api 함수 생성

---
+++ src/services/apis/ranking/index.ts
@@ -0,0 +1,28 @@
+import { SERVER_URL } from '@/services/config/constants';
+import { GetActualSeasonRankingResponse, GetGambleSeasonRankingResponse } from './dto';
+
+export const getGambleSeasonRanking = async (league: number): Promise<GetGambleSeasonRankingResponse | null> => {
+	const params = new URLSearchParams({
+		league: String(league),
+	});
+	const response = await fetch(`${SERVER_URL}/api/gamble-season-ranking?${params}`);
+
+	if (!response.ok) {
+		console.error(await response.json());
+		return null;
+	}
+	return response.json();
+};
+
+export const getActualSeasonRanking = async (league: number): Promise<GetActualSeasonRankingResponse | null> => {
+	const params = new URLSearchParams({
+		league: String(league),
+	});
+	const response = await fetch(`${SERVER_URL}/api/gamble-season-ranking?${params}`);
+
+	if (!response.ok) {
+		console.error(await response.json());
+		return null;
+	}
+	return response.json();
+};
