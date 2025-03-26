8fea9ebf8ccc3f0bf5512a1e1359688895a797a0
young <azerosong@gmail.com>
2025-03-26 20:41:28 +0900

    feat: 게시글 리스트 조회 api fetch 함수 생성

--- src/services/apis/news/dto.ts
+++ src/services/apis/news/dto.ts
@@ -11,12 +11,33 @@ export interface GetNewsListRequest {
 
 export type GetNewsListResponse = BaseSuccessResponse<NewsItemDto[]>;
 
+// 게시글 리스트 조회
+export interface GetBoardListRequest {
+	team?: number;
+	size: number;
+	page: number;
+	order: string;
+}
+
+export type GetBoardListResponse = BaseSuccessResponse<BoardItemDto[]>;
+
+// 내부 DTO
 export interface NewsItemDto {
 	pk: number;
 	title: string;
 	content: string;
 	thumbnailUrl: string;
 	category: string;
+	user: UserDto;
+	createdAt: string;
+	views: number;
+	likes: number;
+	replies: number;
+}
+
+export interface BoardItemDto {
+	pk: number;
+	title: string;
 	user: UserDto;
 	createdAt: string;
 	views: number;
---
+++ src/services/apis/news/getBoardList.ts
@@ -0,0 +1,25 @@
+import { SERVER_URL } from '@/services/config/constants';
+import { GetBoardListRequest, GetBoardListResponse } from './dto';
+
+export const getBoardList = async ({
+	team,
+	size,
+	page,
+	order,
+}: GetBoardListRequest): Promise<GetBoardListResponse | null> => {
+	const params = new URLSearchParams();
+
+	params.append('order', String(order));
+	params.append('size', String(size));
+	params.append('page', String(page));
+
+	if (order) params.append('team', String(team));
+
+	const response = await fetch(`${SERVER_URL}/api/board?${params.toString()}`);
+
+	if (!response.ok) {
+		console.error(await response.json());
+		return null;
+	}
+	return response.json();
+};
