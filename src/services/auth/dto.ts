b80794b6ed0f35a90df700a3e52691a07dd2976d
young <azerosong@gmail.com>
2025-03-27 16:07:33 +0900

    feat: 유저 정보 수정 api 생성

--- src/services/auth/dto.ts
+++ src/services/auth/dto.ts
@@ -2,3 +2,9 @@ export interface UpdatePrivacyRequest {
 	privacyAgreedAt: string;
 	marketingAgreedAt?: string;
 }
+
+export interface UpdateUserInfoRequest {
+	nickname: string;
+	team: number;
+	league: number;
+}
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,9 +1,24 @@
-import type { UpdatePrivacyRequest } from './dto';
-import { EmptySuccessResponse, FailResponse } from '../config/dto';
+import type { UpdatePrivacyRequest, UpdateUserInfoRequest } from './dto';
+import type { EmptySuccessResponse, FailResponse } from '../config/dto';
 import axiosInstance from '../config/axiosInstance';
 
 // 개인정보 동의 업데이트
 export const updatePrivacy = async (body: UpdatePrivacyRequest) => {
+	try {
+		const response = await axiosInstance.patch<EmptySuccessResponse | FailResponse>('/api/user/privacy', body);
+
+		if (response.code !== 'SUCCESS') {
+			console.error(response);
+			return response.message;
+		}
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// 유저 정보 수정
+export const updateUserInfo = async (body: UpdateUserInfoRequest) => {
 	try {
 		const response = await axiosInstance.patch<EmptySuccessResponse | FailResponse>('/api/user/privacy', body);
 
