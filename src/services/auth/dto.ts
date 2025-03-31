efb5bd3e95ce77375e97a2546e9baf41830559c3
young <azerosong@gmail.com>
2025-03-31 17:01:06 +0900

    feat: 리프레시 토큰 발급 api 생성

--- src/services/auth/dto.ts
+++ src/services/auth/dto.ts
@@ -1,16 +1,19 @@
 import { SuccessResponse } from '../config/dto';
 
+// 개인정보 동의
 export interface UpdatePrivacyRequest {
 	privacyAgreedAt: string;
 	marketingAgreedAt?: string;
 }
 
+// 유저 정보 수정
 export interface UpdateUserInfoRequest {
 	nickname: string;
 	team?: number;
 	league?: number;
 }
 
+// 유저 정보 조회
 export type GetUserInfoResponse = SuccessResponse<UserInfoDto>;
 
 export interface UserInfoDto {
@@ -25,4 +28,16 @@ export interface UserInfoDto {
 	leagueName?: string;
 	privacyAgreedAt: string;
 	marketingAgreedAt: string;
+}
+
+// 토큰 재발급
+export interface PostNewTokenRequest {
+	refreshToken: string;
+}
+
+export type PostNewTokenResponse = SuccessResponse<NewTokenDto>;
+
+export interface NewTokenDto {
+	refreshToken: string;
+	accessToken: string;
 }
