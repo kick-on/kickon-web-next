d429928a1e2170076dc4950074cb9672c2185be2
young <azerosong@gmail.com>
2025-04-01 19:41:40 +0900

    fix: searchParams 비동기 문제 해결

--- src/app/(with-side)/board/page.tsx
+++ src/app/(with-side)/board/page.tsx
@@ -1,10 +1,15 @@
 import CategoryTab from '@/components/common/category-tab/category-tab';
 import ComponentFrame from '@/components/common/componentFrame';
 
-export default function Page({ searchParams }: { searchParams: { q: string; type: string; id: string } }) {
-	const q = searchParams.q;
-	const type = searchParams.type;
-	const id = searchParams.id;
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: Promise<{ q: string; type: string; id: string }>;
+}) {
+	const params = await searchParams;
+	const q = params.q;
+	const type = params.type;
+	const id = params.id;
 
 	return (
 		<ComponentFrame isMain={true}>
