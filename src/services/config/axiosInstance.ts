505fa83f8de9a701d882131a4c703785532cb26c
young <azerosong@gmail.com>
2025-04-02 17:05:43 +0900

    feat: accessToken이 만료되었을 경우 refreshToken으로 토큰을 재발급하도록 axios 인터셉터 설정

--- src/services/config/axiosInstance.ts
+++ src/services/config/axiosInstance.ts
@@ -6,6 +6,7 @@ import axios, {
 	InternalAxiosRequestConfig,
 } from 'axios';
 import { SERVER_URL } from './constants';
+import { postNewToken } from '../auth';
 
 interface CustomInstance extends AxiosInstance {
 	interceptors: {
@@ -46,9 +47,38 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
 	(response) => response.data,
-	(error) => {
+	async (error) => {
+		const originalRequest = error.config;
+
 		if (error.response) {
-			return error.response.data; // FailResponse
+			// 인증 실패 시
+			if ((error.response.status === 401 || error.response.status === 403) && !originalRequest._retry) {
+				originalRequest._retry = true; // 무한 루프 방지
+
+				const prevRefreshToken = localStorage.getItem('refreshToken');
+
+				// 리프레시 토큰이 있으면 토큰 재발급 후 다시 시도
+				if (prevRefreshToken) {
+					const newTokenResponse = await postNewToken({ refreshToken: prevRefreshToken });
+
+					// 토큰 재발급 성공
+					if (newTokenResponse && typeof newTokenResponse !== 'string') {
+						// 새 토큰 저장
+						localStorage.setItem('refreshToken', newTokenResponse.refreshToken);
+						localStorage.setItem('accessToken', newTokenResponse.accessToken);
+
+						// 요청 재시도
+						originalRequest.headers.Authorization = `Bearer ${newTokenResponse.accessToken}`;
+						return axiosInstance(originalRequest);
+					} else {
+						// 토큰 재발급 실패
+						localStorage.clear();
+					}
+				}
+			}
+
+			// 그 외 서버 응답이 있는 오류 (FailResponse)
+			return error.response.data;
 		}
 		return Promise.reject(error); // 기타 오류
 	},
