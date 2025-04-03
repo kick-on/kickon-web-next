125554689b92796b0928a9e17cb5431167ad77fc
지민재 <jim400934@gmail.com>
2025-04-03 19:03:05 +0900

    fix: news-item의 보이는 콘텐츠의 태그 스타일 제거 및 2줄 제한

--- src/components/common/sanitized-content.tsx
+++ src/components/common/sanitized-content.tsx
@@ -9,15 +9,23 @@ export default function SanitizedContent({ content }: { content: string }) {
 	const [sanitizedContent, setSanitizedContent] = useState('');
 
 	useEffect(() => {
-		setSanitizedContent(DOMPurify.sanitize(content, { FORBID_TAGS: ['img', 'strong', 'p'] }));
+		let cleanText = DOMPurify.sanitize(content, { ALLOWED_TAGS: ['p'] })
+			.replace(/<\/p>\s*<p>/g, '<br />') // 단락 사이 줄 바꿈 유지
+			.replace(/^<p>|<\/p>$/g, ''); // 맨 앞뒤 <p> 제거
+
+		// 2줄까지만 표시 (대략 120자 기준, <br> 포함)
+		if (cleanText.length > 120) {
+			cleanText = cleanText.substring(0, 117) + '...';
+		}
+
+		setSanitizedContent(cleanText);
 	}, [content]);
 
 	return (
 		<div
-			className="mb-[1.125rem] subtitle2-regular font-normal"
-			dangerouslySetInnerHTML={{
-				__html: sanitizedContent.length > 120 ? `${sanitizedContent.substring(0, 117)}...` : sanitizedContent,
-			}}
+			className="mb-[1.125rem] subtitle2-regular font-normal line-clamp-2"
+			style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden' }}
+			dangerouslySetInnerHTML={{ __html: sanitizedContent }}
 		/>
 	);
 }
