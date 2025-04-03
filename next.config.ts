f7bb8968e75d5fea49155f320a7297d998f5bddb
young <azerosong@gmail.com>
2025-04-03 17:57:50 +0900

    chore: image 도메인 저장 방식 변경

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,39 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: [
-			'k.kakaocdn.net',
-			'premierskillsenglish.britishcouncil.org',
-			'media.gettyimages.com',
-			'kickon-files-bucket.s3.ap-northeast-2.amazonaws.com',
-			'media.api-sports.io',
-			'i.ytimg.com',
-			'img1.kakaocdn.net',
-			'ssl.pstatic.net',
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'k.kakaocdn.net',
+			},
+			{
+				protocol: 'https',
+				hostname: 'premierskillsenglish.britishcouncil.org',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.gettyimages.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'kickon-files-bucket.s3.ap-northeast-2.amazonaws.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.api-sports.io',
+			},
+			{
+				protocol: 'https',
+				hostname: 'i.ytimg.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'img1.kakaocdn.net',
+			},
+			{
+				protocol: 'https',
+				hostname: 'ssl.pstatic.net',
+			},
 		],
 	},
 };
