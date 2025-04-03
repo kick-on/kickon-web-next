ebcad7f348c67bd24f884a53f4610cbcabc16900
young <azerosong@gmail.com>
2025-04-03 11:46:48 +0900

    fix: 프로필 컴포넌트 세션스토리지 접근을 useEffect 내부에서 처리

--- src/components/layouts/with-side-layout/profile.tsx
+++ src/components/layouts/with-side-layout/profile.tsx
@@ -24,7 +24,6 @@ export default function Profile() {
 	const fullUrl = !!searchParams.get('login')
 		? '/'
 		: `${pathname}${searchParams.toString() ? '?' + searchParams.toString() : ''}`;
-	sessionStorage.setItem('previousPage', fullUrl);
 
 	const handleLoginButtonClick = () => {
 		setIsLoginModalOpen(true);
@@ -41,6 +40,10 @@ export default function Profile() {
 
 		router.push('/');
 	};
+
+	useEffect(() => {
+		sessionStorage.setItem('previousPage', fullUrl);
+	}, [fullUrl]);
 
 	useEffect(() => {
 		// 저장된 유저 정보가 없으면 jwt 기반으로 유저 정보 불러와 전역 상태 관리
