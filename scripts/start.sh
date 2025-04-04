625c0ca7aa6b43a5b692a36630004d9a1df0d09f
Minseo Lim <limms1217@naver.com>
2025-04-04 09:53:09 +0900

    action: zip 파일 주소 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,6 @@
 cd /home/ubuntu/web-nextjs-service
 
 # PM2 실행 (sudo 사용)
-sudo pm2 delete my-front-app --silent || true
-sudo pm2 start yarn --name "my-front-app" -- start
-sudo pm2 save
+pm2 delete my-front-app --silent || true
+pm2 start yarn --name "my-front-app" -- start
+pm2 save
