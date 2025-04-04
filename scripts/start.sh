0ce244c248922f7032010c249c97d78cb58b8634
Minseo Lim <limms1217@naver.com>
2025-04-04 09:58:21 +0900

    deploy: pm2 path 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/web-nextjs-service
 
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
 # PM2 실행 (sudo 사용)
 pm2 delete my-front-app --silent || true
 pm2 start yarn --name "my-front-app" -- start
