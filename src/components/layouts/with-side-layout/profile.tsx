35776127f6c4c38c968d35f8350470bffa09348b
young <azerosong@gmail.com>
2025-04-08 18:27:44 +0900

    fix: 프로필 컴포넌트 window 참조 오류 해결

--- src/components/layouts/with-side-layout/profile.tsx
+++ src/components/layouts/with-side-layout/profile.tsx
@@ -16,7 +16,7 @@ export default function Profile() {
 	const pathname = usePathname();
 	const searchParams = useSearchParams();
 
-	const previousPage = window ? sessionStorage.getItem('previousPage') : null;
+	const previousPage = typeof window !== 'undefined' ? sessionStorage.getItem('previousPage') : null;
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [isLoginModalOpen, setIsLoginModalOpen] = useState(!!searchParams.get('login'));
 
