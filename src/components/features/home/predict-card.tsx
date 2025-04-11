ede92f769d55debe914e6e2647f09df1dd3e90ae
young <azerosong@gmail.com>
2025-04-11 22:21:51 +0900

    feat: 참여한 모든 완료된 경기에 대해 내 예측 보이기(예측 성공 시에도)

--- src/components/features/home/predict-card.tsx
+++ src/components/features/home/predict-card.tsx
@@ -23,6 +23,8 @@ export default function PredictCard({
 	const participations = formatGambleParticipations(gambleResult.participationNumber);
 	const timeBefore = getGameStartTimeBefore(startAt);
 
+	const isGameCompleted = gameStatus === 'HOME' || gameStatus === 'DRAW' || gameStatus === 'AWAY';
+
 	return (
 		<div
 			className={clsx(
@@ -31,17 +33,20 @@ export default function PredictCard({
 			)}
 		>
 			<div className="flex justify-between items-center">
-				<div className="subtitle1-semibold flex gap-2 items-center">
+				<div className="subtitle1-semibold flex items-center">
 					{leagueName || '리그'}
 					<div
-						className={clsx('px-2 py-1 rounded-full text-black-000 caption2-regular text-center items-center', {
-							'bg-black-900': gameStatus === 'PENDING',
-							'bg-primary-900':
-								myGambleResult &&
-								(myGambleResult.gambleStatus === 'SUCCEED' || myGambleResult.gambleStatus === 'PERFECT'),
-							'bg-black-700': myGambleResult && myGambleResult.gambleStatus === 'FAILED',
-							'bg-black-500': !myGambleResult || gameStatus === 'CANCELED',
-						})}
+						className={clsx(
+							'px-2 py-1 ml-2 mr-0.5 rounded-full text-black-000 caption2-regular text-center items-center',
+							{
+								'bg-black-900': gameStatus === 'PENDING',
+								'bg-primary-900':
+									myGambleResult &&
+									(myGambleResult.gambleStatus === 'SUCCEED' || myGambleResult.gambleStatus === 'PERFECT'),
+								'bg-black-700': myGambleResult && myGambleResult.gambleStatus === 'FAILED',
+								'bg-black-500': !myGambleResult || gameStatus === 'CANCELED',
+							},
+						)}
 					>
 						{gameStatus === 'PENDING'
 							? '예측 진행 중'
@@ -50,9 +55,14 @@ export default function PredictCard({
 								: myGambleResult.gambleStatus === 'SUCCEED'
 									? '예측 성공'
 									: myGambleResult.gambleStatus === 'FAILED'
-										? `예측 실패 ${myGambleResult.homeScore}:${myGambleResult.awayScore}`
+										? `예측 실패`
 										: ''}
 					</div>
+					{isGameCompleted && myGambleResult && (
+						<div className="caption2-regular text-black-700">
+							・내 예측 {myGambleResult.homeScore}:{myGambleResult.awayScore}
+						</div>
+					)}
 				</div>
 				{gameStatus === 'PENDING' && <div className="caption1-regular text-black-700">마감 {timeBefore}</div>}
 			</div>
@@ -70,7 +80,9 @@ export default function PredictCard({
 								? '풀타임'
 								: gameStatus === 'CANCELED'
 									? '경기 취소'
-									: '기타'}
+									: gameStatus === 'POSTPONED'
+										? '경기 지연'
+										: '기타'}
 					</div>
 					<div className={clsx('button6-regular', { 'line-through': gameStatus === 'CANCELED' })}>{startDate}</div>
 					<div className={clsx('button6-regular', { 'line-through': gameStatus === 'CANCELED' })}>{startTime}</div>
