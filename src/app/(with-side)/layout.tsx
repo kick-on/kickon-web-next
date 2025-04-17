4c52cb7191628cad8ea467f5b3ab64b6d630194a
young <azerosong@gmail.com>
2025-04-17 20:50:50 +0900

    feat: with side 레이아웃 반응형 적용

--- src/app/(with-side)/layout.tsx
+++ src/app/(with-side)/layout.tsx
@@ -4,22 +4,22 @@ import Profile from '@/components/layouts/with-side/profile';
 import RankingList from '@/components/layouts/with-side/ranking-list/ranking-list';
 import { ReactNode, Suspense } from 'react';
 import clsx from 'clsx';
-import getDeviceType from '@/lib/utils/getDeviceType';
+import getServerDeviceType from '@/lib/utils/getServerDeviceType';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-	const { isMobile } = await getDeviceType();
+	const { isMobile } = await getServerDeviceType();
 
 	return (
-		<div className={clsx('pt-4 max-w-[85rem] m-auto grid gap-6', { 'grid-cols-[1fr_auto_1fr]': !isMobile })}>
+		<div className={clsx('pt-4 max-w-[85rem] m-auto grid gap-6 not-tablet:grid-cols-[1fr_auto_1fr]')}>
 			{!isMobile && (
-				<aside className="flex flex-col gap-4">
+				<aside className="tablet:hidden flex flex-col gap-4">
 					<RankingList mode="season" />
 					<RankingList mode="predict" />
 				</aside>
 			)}
-			<main className="flex flex-col gap-4">{children}</main>
+			<main className="flex flex-col items-center gap-4">{children}</main>
 			{!isMobile && (
-				<aside className="flex flex-col gap-4 relative">
+				<aside className="tablet:hidden flex flex-col gap-4 relative">
 					<Suspense>
 						<Profile />
 					</Suspense>
@@ -27,6 +27,7 @@ export default async function Layout({ children }: { children: ReactNode }) {
 					<FloatingWritingButton />
 				</aside>
 			)}
+			{isMobile && <FloatingWritingButton />}
 		</div>
 	);
 }
--- src/lib/utils/getServerDeviceType.ts
+++ src/lib/utils/getServerDeviceType.ts
@@ -1,18 +1,14 @@
 import { headers } from 'next/headers';
 import { UAParser } from 'ua-parser-js';
 
-export default async function getDeviceType() {
-	let device: UAParser.IDevice | null = null;
+export default async function getServerDeviceType() {
+	if (typeof window !== 'undefined') return;
 
-	if (typeof window === 'undefined') {
-		// 서버사이드
-		const headerList = await headers();
-		const uaString = headerList.get('user-agent') ?? '';
-		device = UAParser(uaString).device;
-	} else {
-		// 클라이언트 사이드
-		device = UAParser().device;
-	}
+	// 서버사이드
+	const headerList = await headers();
+	const uaString = headerList.get('user-agent') ?? '';
+	const device = UAParser(uaString).device;
+
 	// 모바일 데스크톱 외 나머지는 모두 데스크톱으로 취급
 	const isMobile = device.type === 'mobile';
 	const isTablet = device.type === 'tablet';
