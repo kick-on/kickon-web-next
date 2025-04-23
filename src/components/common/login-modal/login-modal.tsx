f829d52af37245f4391d92d210c3c5137bff799d
young <azerosong@gmail.com>
2025-04-23 13:57:25 +0900

    feat: 로그인 모달 모바일 적응형 구현

--- src/components/common/login-modal/login-modal.tsx
+++ src/components/common/login-modal/login-modal.tsx
@@ -27,33 +27,39 @@ export default function LoginModal({ onClose }) {
 			}
 		};
 
+		document.body.classList.add('overflow-hidden');
 		document.addEventListener('click', handleOutsideClick);
-		document.body.style.overflow = 'hidden';
 
 		return () => {
+			document.body.classList.remove('overflow-hidden');
 			document.removeEventListener('click', handleOutsideClick);
-			document.body.style.overflow = 'unset';
 		};
-	}, [onClose, router]);
+	}, [onClose]);
 
 	return (
-		<div className="fixed z-50 flex justify-center items-center top-0 left-0 w-full h-full bg-black/40">
+		<div className="fixed z-50 @mobile:z-60 flex justify-center items-center top-0 left-0 w-full h-full bg-black/40">
 			<div
 				ref={modalRef}
 				className="flex flex-col items-center p-6 shadow-predict-button
-            w-[39.5rem] h-[39.75rem] bg-black-000 rounded-[0.625rem] display-semibold"
+					w-[39.5rem] h-[39.75rem] bg-black-000 rounded-[0.625rem] display-semibold
+					@mobile:w-full @mobile:h-full @mobile:rounded-none @mobile:text-20 @mobile:font-semibold @mobile:leading-8"
 			>
-				<button onClick={handleXButtonClick} className="ml-auto mb-[4.6875rem]">
+				<button onClick={handleXButtonClick} className="ml-auto mb-[4.6875rem] @mobile:mt-2 @mobile:-mb-8">
 					<Image width={24} height={24} src="/x.svg" alt="닫기" />
 				</button>
-				<Image width={280} height={62} src="/logo/kick-on-red.svg" alt="킥온 로고" />
 
-				<div className="mt-[1.875rem]">로그인하고 킥온과 함께</div>
-				<div>좋아하는 축구팀 승부예측하러 가요!</div>
+				<div className="flex flex-col items-center @mobile:my-auto">
+					<div className="relative w-70 h-[3.875rem] @mobile:w-54 @mobile:h-12">
+						<Image fill src="/logo/kick-on-red.svg" alt="킥온 로고" />
+					</div>
 
-				<div className="mt-[6.625rem] flex flex-col gap-5">
-					<LoginButton social="카카오" onClick={() => handleLoginButtonClick('kakao')} />
-					<LoginButton social="네이버" onClick={() => handleLoginButtonClick('naver')} />
+					<div className="mt-[1.875rem]">로그인하고 킥온과 함께</div>
+					<div>좋아하는 축구팀 승부예측하러 가요!</div>
+
+					<div className="mt-[6.625rem] flex flex-col gap-5">
+						<LoginButton social="카카오" onClick={() => handleLoginButtonClick('kakao')} />
+						<LoginButton social="네이버" onClick={() => handleLoginButtonClick('naver')} />
+					</div>
 				</div>
 			</div>
 		</div>
