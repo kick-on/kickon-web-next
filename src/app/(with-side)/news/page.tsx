044062172c01238b6444de3c1cb9b195d0cd8945
young <azerosong@gmail.com>
2025-04-24 21:36:05 +0900

    feat: 뉴스/게시글 목록 페이지 fallback 설정

--- src/app/(with-side)/news/page.tsx
+++ src/app/(with-side)/news/page.tsx
@@ -1,5 +1,7 @@
 import CategoryTab from '@/components/common/category-tab/category-tab';
+import EmptyState from '@/components/common/category-tab/empty-state';
 import ComponentFrame from '@/components/common/component-frame';
+import { Suspense } from 'react';
 
 export default async function Page({
 	searchParams,
@@ -14,7 +16,9 @@ export default async function Page({
 
 	return (
 		<ComponentFrame className="@mobile:mb-[80px]" isMain={true}>
-			<CategoryTab mode="news" q={q} type={type} id={id} page={page} />
+			<Suspense fallback={<EmptyState isNews={true} />}>
+				<CategoryTab mode="news" q={q} type={type} id={id} page={page} />
+			</Suspense>
 		</ComponentFrame>
 	);
 }
--- src/components/common/category-tab/category-tab.tsx
+++ src/components/common/category-tab/category-tab.tsx
@@ -6,9 +6,8 @@ import { getBoardList } from '@/services/apis/board/getBoardList';
 import FetchingFailedCard from '../fetching-failed-card';
 import TabBar from './tab-bar';
 import PaginationBar from '../pagination-bar';
-import Image from 'next/image';
-import Link from 'next/link';
 import clsx from 'clsx';
+import EmptyState from './empty-state';
 
 const renderItems = (items, ItemComponent) => (
 	<div>
@@ -54,17 +53,7 @@ export default async function CategoryTab({
 			{!response ? (
 				<FetchingFailedCard height="770px" marginTop="9.5rem" />
 			) : !response.data.length ? (
-				<div className="flex flex-col mt-[9.9375rem] items-center">
-					<Image width={120} height={74} src={'/goal-post.svg'} alt={'골대 이미지'} />
-					<span className="mt-[2.375rem] mb-4 body2-semibold">아직 작성된 게시글이 없어요.</span>
-					<span className="mb-9 body5-regular">{isNews ? '뉴스' : '클럽 커뮤니티'} 게시글의 첫 키커가 되어주세요!</span>
-					<Link
-						className="flex gap-1.5 body7-regular text-black-700 mb-[30.625rem]"
-						href={`/post/${isNews ? 'news' : 'board'}`}
-					>
-						작성하러 가기 <Image width={16} height={16} src={'/chevron/right-gray.svg'} alt="바로가기" />
-					</Link>
-				</div>
+				<EmptyState isNews={isNews} />
 			) : (
 				<div className="flex flex-col w-full pb-10 @mobile:pb-0">
 					{renderItems(response.data, isNews ? NewsItem : CommunityItem)}
