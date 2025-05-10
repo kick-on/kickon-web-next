166f5e8b7f240be555cbd0f46cfebbd94c6eb84a
지민재 <mimizae@jiminjaeui-MacBookPro.local>
2025-05-10 17:35:29 +0900

    refact: 본문 컨텐츠가 존재할 때 작성 취소 버튼 클릭 시 alert

--- src/app/(without-side)/post/news/page.tsx
+++ src/app/(without-side)/post/news/page.tsx
@@ -369,7 +369,12 @@ export default function Page() {
 
 			<div className="flex justify-center gap-4 mt-4">
 				<button
-					onClick={() => router.back()}
+					onClick={() => {
+						const confirmCancel = window.confirm('게시글 작성을 취소하겠습니까?');
+						if (confirmCancel) {
+							router.back();
+						}
+					}}
 					className="w-41 @mobile:w-37 button2-semibold @mobile:text-15 px-4 py-2 rounded-lg transition-all text-black-700 bg-black-200"
 				>
 					취소
