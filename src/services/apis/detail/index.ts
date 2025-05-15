8e25c40af01bf033af67214e4846380ea2f4caef
young <azerosong@gmail.com>
2025-05-15 17:17:57 +0900

    feat: detail 관련 fetcher 함수 적용

--- src/services/apis/detail/index.ts
+++ src/services/apis/detail/index.ts
@@ -1,9 +1,9 @@
-import axiosInstance from '@/services/config/axiosInstance';
 import { GetDetailResponse } from './dto';
+import { fetcher } from '@/lib/server/fetcher';
 
 export const getDetailContent = async (type: 'news' | 'board', id: number): Promise<GetDetailResponse | null> => {
 	try {
-		const response = await axiosInstance.get<GetDetailResponse>(`/api/${type}/${id}`);
+		const response = await fetcher<GetDetailResponse>({ method: 'GET', url: `/api/${type}/${id}` });
 
 		if (!response) {
 			console.error('상세페이지 조회 실패 - 응답 없음');
