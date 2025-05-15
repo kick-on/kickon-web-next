d6d91ed073dd03e9afdcc55ccad1270b28d63d38
young <azerosong@gmail.com>
2025-05-15 15:00:34 +0900

    feat: proxy 토큰 재발급 로직 추가

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,6 +1,5 @@
-// app/api/proxy/route.ts
-
-import { DOMAIN_URL, SERVER_URL } from '@/services/config/constants';
+import callBackendApi from '@/lib/server/callBackendApi';
+import { DOMAIN_URL } from '@/services/config/constants';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
@@ -14,38 +13,25 @@ export interface ProxyParameter {
 export async function POST(req: Request) {
 	const { method, endpoint, headers, body }: ProxyParameter = await req.json();
 
+	// 요청을 시도한 주소
 	const requestOrigin = req.headers.get('origin');
-	const allowedOrigin = DOMAIN_URL;
 
-	// 다른 origin에서 호출 시 에러 반환 (csrf 방어)
-	if (!requestOrigin || requestOrigin !== allowedOrigin) {
+	// origin이 비어있지 않고 (일부 ssr 등에서는 origin이 비어 있을 수 있음)
+	// 해당 origin이 DOMAIN URL과 일치하지 않다면 (proxy를 통한 외부 공격 방어)
+	if (requestOrigin && requestOrigin !== DOMAIN_URL) {
 		return NextResponse.json({ message: 'Forbidden: invalid origin' }, { status: 403 });
 	}
 
-	// 쿠키에서 액세스 토큰을 읽음
 	const cookieStore = await cookies();
-	const accessToken = cookieStore.get('accessToken'); // 쿠키에서 액세스 토큰을 가져옵니다.
-	const hasToken = !!accessToken;
+	const accessToken = cookieStore.get('accessToken')?.value;
+	const refreshToken = cookieStore.get('refreshToken')?.value;
 
-	try {
-		// 백엔드 API 호출
-		const hasBody = body !== undefined;
-		const apiResponse = await fetch(`${SERVER_URL}${endpoint}`, {
-			method: method,
-			headers: {
-				...(hasToken ? { Authorization: `Bearer ${accessToken.value}` } : {}), // 토큰이 있는 경우 헤더에 토큰을 추가
-				...(hasBody ? { 'Content-Type': 'application/json' } : {}), // body가 있는 경우 헤더에 속성 추가
-				...headers, // 기타 추가 헤더 설정
-			},
-			body: hasBody ? JSON.stringify(body) : undefined, // body가 있는 경우 바디 추가
-		});
-
-		const data = await apiResponse.json();
-
-		// 백엔드 API 응답을 클라이언트에 전달
-		return NextResponse.json(data, { status: apiResponse.status });
-	} catch (error) {
-		console.error('Error fetching from backend:', error);
-		return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-	}
+	return await callBackendApi({
+		method,
+		endpoint,
+		headers,
+		body,
+		accessToken,
+		refreshToken,
+	});
 }
---
+++ src/lib/server/callBackendApi.ts
@@ -0,0 +1,89 @@
+import { ProxyParameter } from '@/app/api/proxy/route';
+import { SERVER_URL } from '@/services/config/constants';
+import { NextResponse } from 'next/server';
+
+export default async function callBackendApi({
+	method,
+	endpoint,
+	headers,
+	body,
+	accessToken,
+	refreshToken,
+	retry = false,
+}: ProxyParameter & { accessToken: string; refreshToken?: string; retry?: boolean }) {
+	const hasBody = !!body;
+
+	// 백엔드 api 호출
+	const response = await fetch(`${SERVER_URL}${endpoint}`, {
+		method,
+		headers: {
+			...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+			...(hasBody ? { 'Content-Type': 'application/json' } : {}),
+			...headers,
+		},
+		body: hasBody ? JSON.stringify(body) : undefined,
+	});
+
+	// 첫 번째 api 호출에서(무한 루프 방지)
+	// 인증 에러가 뜨고, refreshToken이 있는 경우
+	// 토큰 재발급
+	if (!retry && (response.status === 401 || response.status === 403) && refreshToken) {
+		const refreshResponse = await fetch(`${SERVER_URL}/auth/refresh`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ refreshToken }),
+		});
+
+		// 재발급 성공
+		if (refreshResponse.ok) {
+			const { data } = await refreshResponse.json();
+			const { accessToken: newAccessToken, refreshToken: newRefreshToken } = data;
+
+			// api 다시 호출
+			const retriedResponse = await callBackendApi({
+				method,
+				endpoint,
+				headers,
+				body,
+				accessToken: newAccessToken,
+				refreshToken: newRefreshToken,
+				retry: true,
+			});
+
+			// retriedResponse에 쿠키를 추가
+			if (typeof newAccessToken === 'string') {
+				retriedResponse.cookies.set({
+					name: 'accessToken',
+					value: newAccessToken,
+					httpOnly: true,
+					secure: true,
+					path: '/',
+					maxAge: 60 * 60,
+				});
+			}
+
+			if (typeof newRefreshToken === 'string') {
+				retriedResponse.cookies.set({
+					name: 'refreshToken',
+					value: newRefreshToken,
+					httpOnly: true,
+					secure: true,
+					path: '/',
+					maxAge: 60 * 60 * 24 * 30,
+				});
+			}
+
+			// 재시도 api 응답 (새 토큰을 정상적으로 발급받아 다시 시도한 경우)
+			return retriedResponse;
+		} else {
+			// 재발급 실패
+			// 토큰 발급 에러 응답 (새 토큰 발급에 실패한 경우)
+			return NextResponse.json({ message: 'Token refresh failed' }, { status: 401 });
+		}
+	}
+
+	// 기본 api 응답 (새 토큰 발급을 시도하지 않은 경우)
+	// ex. accessToken이 정상적으로 있었던 경우 또는 refreshToken 자체가 없는 경우 등
+	const data = await response.json();
+	return NextResponse.json(data, { status: response.status });
+}
