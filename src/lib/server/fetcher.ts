bd0c161b3f11c2a82cd1891b6d0688c635854c53
young <azerosong@gmail.com>
2025-05-15 17:08:07 +0900

    fix: fetcher 함수에서 proxy를 절대경로로 수정

--- src/lib/server/fetcher.ts
+++ src/lib/server/fetcher.ts
@@ -1,8 +1,9 @@
 import { ProxyParameter } from '@/app/api/proxy/route';
+import { DOMAIN_URL } from '@/services/config/constants';
 
 // proxy 경로로 라우팅하는 함수
 export async function fetcher<T>(body: ProxyParameter, revalidate?: number) {
-	const response = await fetch('/api/proxy', {
+	const response = await fetch(`${DOMAIN_URL}/api/proxy`, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(body),
