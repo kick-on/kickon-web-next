8dc04ab1488b901c7df80886f412654dd4f7e4d8
young <azerosong@gmail.com>
2025-05-15 17:28:50 +0900

    fix: 에러 코드가 없을 때에도 alert가 뜨는 문제 해결

--- src/app/auth/finalize/page.tsx
+++ src/app/auth/finalize/page.tsx
@@ -11,8 +11,9 @@ export default function Page() {
 	useEffect(() => {
 		const previousPage = sessionStorage.getItem('previousPage');
 
-		// 에러 코드가 있으면 적절한 alert
-		if (errorCode === 'REJOIN_LIMIT') {
+		if (!errorCode) {
+			// 에러 코드가 없으면 아무 동작도 안 함함
+		} else if (errorCode === 'REJOIN_LIMIT') {
 			alert('탈퇴 후 7일이 지나지 않아 재가입할 수 없습니다.');
 		} else {
 			alert('알 수 없는 오류가 발생했습니다.');
