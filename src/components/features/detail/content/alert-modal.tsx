f7c43483242236a1e636f2b6a053926614110542
지민재 <mimizae@jiminjaeui-MacBookPro.local>
2025-05-16 13:30:18 +0900

    feat: 더보기 버튼 영역 css 상세 조정

---
+++ src/components/features/detail/content/alert-modal.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import useIsMobile from '@/lib/hooks/useIsMobile';
+import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
+
+interface AlertModalProps {
+	type?: 'confirm' | 'info';
+	description: string;
+	confirmText?: string;
+	cancelText?: string;
+	onConfirm?: () => void;
+	onCancel?: () => void;
+}
+
+const AlertModal = ({
+	type = 'confirm',
+	description,
+	confirmText = '확인',
+	cancelText = '취소',
+	onConfirm,
+	onCancel,
+}: AlertModalProps) => {
+	const isMobile = useIsMobile();
+	const confirmButtonRef = useRef<HTMLButtonElement>(null);
+
+	useEffect(() => {
+		if (type === 'confirm') {
+			const timer = setTimeout(() => {
+				confirmButtonRef.current?.focus();
+			}, 100);
+			return () => clearTimeout(timer);
+		}
+	}, [type]);
+
+	const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (e.target === e.currentTarget && onCancel) {
+			onCancel();
+		}
+	};
+
+	return (
+		<div onClick={handleBackdropClick} className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+			<div
+				onClick={(e) => e.stopPropagation()}
+				className={clsx(
+					'flex flex-col justify-center bg-black-000 rounded-lg px-7 w-[344px] @mobile:w-77.75',
+					isMobile ? 'pt-[50px] pb-8 gap-[42px]' : 'pt-[68px] pb-[36px] gap-[54px]',
+				)}
+			>
+				<p className={clsx('body2-semibold @mobile:text-18 text-center text-black-900', type === 'info' && 'text-lg')}>
+					{description}
+				</p>
+
+				<div
+					className={clsx(
+						'button2-semibold @mobile:text-15 w-full flex',
+						type === 'confirm' ? 'justify-end gap-4' : 'justify-center',
+					)}
+				>
+					{type === 'confirm' ? (
+						<>
+							<button onClick={onCancel} className="w-1/2 p-[10px] bg-black-200 text-black-700 rounded-md">
+								{cancelText}
+							</button>
+							<button
+								ref={confirmButtonRef}
+								onClick={onConfirm}
+								className="w-1/2 px-4 py-2.5 bg-primary-900 text-black-000 rounded-md"
+							>
+								{confirmText}
+							</button>
+						</>
+					) : (
+						<button
+							ref={confirmButtonRef}
+							onClick={onCancel}
+							className="w-full px-4 py-2.5 bg-primary-900 text-black-000 rounded-md"
+						>
+							{confirmText}
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AlertModal;
--- src/components/features/detail/content/more-actions-button.tsx
+++ src/components/features/detail/content/more-actions-button.tsx
@@ -4,10 +4,9 @@ import { useState, useRef, useEffect, FC } from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import ReportModal from './report-modal';
-import ConfirmModal from './confirm-modal';
 import EditIcon from '@/assets/edit.svg';
-
-// TODO: url 공유 되고 나서 모달 띄우기
+import AlertModal from './alert-modal';
+import { useRouter } from 'next/navigation';
 
 interface MoreActionsButtonProps {
 	type: 'news' | 'board';
@@ -15,10 +14,14 @@ interface MoreActionsButtonProps {
 	isMyContent: boolean;
 }
 
+// 뉴스를 삭제하는지, 커뮤니티를 삭제하는지 -> api 호출에 쓰고 라우팅에 쓰기
+
 const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }) => {
+	const router = useRouter();
 	const [isOpen, setIsOpen] = useState(false);
 	const [showReportModal, setShowReportModal] = useState(false);
-	const [showDeleteConfirm, setShowDeleteConfirm] = useState(false); // 삭제 모달 상태 추가
+	const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+	const [showShareAlert, setShowShareAlert] = useState(false);
 	const menuRef = useRef<HTMLDivElement>(null);
 	const buttonRef = useRef<HTMLButtonElement>(null);
 
@@ -39,9 +42,9 @@ const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }
 	const handleShareButtonClick = async () => {
 		try {
 			await navigator.clipboard.writeText(window.location.href);
-			alert('URL이 복사되었습니다!');
+			setShowShareAlert(true); // 성공 시 모달 표시
 		} catch (err) {
-			console.log(err);
+			console.error(err);
 			alert('URL 복사에 실패했습니다.');
 		}
 		setIsOpen(false);
@@ -54,12 +57,13 @@ const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }
 
 	const handleDeleteClick = () => {
 		setIsOpen(false);
-		setShowDeleteConfirm(true); // 삭제 모달 열기
+		setShowDeleteConfirm(true);
 	};
 
 	const handleConfirmDelete = () => {
-		console.log('삭제.');
-		setShowDeleteConfirm(false);
+		console.log('삭제.'); // api 호출
+		setShowDeleteConfirm(false); // 모달 닫고
+		router.push(`/${type}?q=전체`);
 	};
 
 	return (
@@ -79,13 +83,14 @@ const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }
 					<div
 						ref={menuRef}
 						className={clsx(
-							'absolute @mobile:right-0 mt-2 items-center shadow-[0_4px_10px_0_rgba(0,0,0,0.16)]',
-							'w-[7.1875rem] bg-black-000 rounded-lg border border-black-300 z-50 flex flex-col px-5 button4-medium text-black-900',
-							isMyContent ? 'py-4 gap-[30px]' : 'py-[11px] gap-5',
+							'absolute z-50 mt-2 flex flex-col items-center rounded-lg border border-black-300 bg-black-000 text-black-900 shadow-[0_4px_10px_0_rgba(0,0,0,0.16)]',
+							'w-[7.1875rem] px-5 py-[11px] button4-medium',
+							'@mobile:right-0 @mobile:pr-6 @mobile:py-4',
+							!isMyContent && 'gap-5',
 						)}
 					>
 						{isMyContent ? (
-							<div className="flex flex-col gap-[30px]">
+							<div className="flex flex-col gap-5 @mobile:gap-[30px]">
 								<button className="flex items-center gap-2 whitespace-nowrap" onClick={handleDeleteClick}>
 									<EditIcon alt="수정하기 버튼" width={18} height={18} className="w-[18px] h-[18px] stroke-black-600" />
 									수정하기
@@ -101,7 +106,9 @@ const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }
 								신고하기
 							</button>
 						)}
-
+						{isMyContent && (
+							<p className="w-[99px] h-[1px] border border-black-200 -mr-1 my-[12px] @mobile:my-[16px]" />
+						)}
 						<button className="flex items-center gap-2 whitespace-nowrap" onClick={handleShareButtonClick}>
 							<Image src="/share.svg" alt="공유하기 버튼" width={18} height={18} />
 							공유하기
@@ -113,11 +120,14 @@ const MoreActionsButton: FC<MoreActionsButtonProps> = ({ type, pk, isMyContent }
 			{showReportModal && <ReportModal type={type} pk={pk} onClose={() => setShowReportModal(false)} />}
 
 			{showDeleteConfirm && (
-				<ConfirmModal
-					description="게시글을 삭제하시겠습니까?"
+				<AlertModal
+					description="게시글을 삭제할까요?"
 					onConfirm={handleConfirmDelete}
 					onCancel={() => setShowDeleteConfirm(false)}
 				/>
+			)}
+			{showShareAlert && (
+				<AlertModal type="info" description="URL이 복사되었습니다." onCancel={() => setShowShareAlert(false)} />
 			)}
 		</>
 	);
