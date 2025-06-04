295ac491a072850bcb85575971aa6c74df85bca1
young <azerosong@gmail.com>
2025-06-04 20:35:17 +0900

    feat: 유저 상호작용에 따른 ui 구현

--- src/components/common/account/favorite-team-item.tsx
+++ src/components/common/account/favorite-team-item.tsx
@@ -1,12 +1,22 @@
 'use client';
 
+import { TeamDto } from '@/services/apis/team/dto';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { useState } from 'react';
 
-export default function FavoriteTeamItem({ orderNum }: { orderNum: number }) {
-	const [isActive, setIsActive] = useState(true);
-
+export default function FavoriteTeamItem({
+	team,
+	orderNum,
+	isActive,
+	onClickItem,
+	onClickXButton,
+}: {
+	team: TeamDto | null;
+	orderNum: number;
+	isActive: boolean;
+	onClickItem: () => void;
+	onClickXButton: (e: React.MouseEvent) => void;
+}) {
 	return (
 		<div className="grow space-y-1">
 			<div
@@ -15,28 +25,47 @@ export default function FavoriteTeamItem({ orderNum }: { orderNum: number }) {
 			>
 				{orderNum}
 			</div>
+
 			<div
+				role="button"
+				tabIndex={0}
+				onClick={onClickItem}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter' || e.key === ' ') {
+						onClickItem();
+					}
+				}}
 				className={clsx(
-					`relative w-full h-auto aspect-[5/4]
+					`relative w-full h-auto aspect-[5/4] cursor-pointer
           flex flex-col gap-1 justify-center items-center rounded-lg bg-black-000`,
 					isActive ? 'p-[4px] pb-[2px] border-2 border-primary-900' : 'p-[5px] pb-[3px] border border-black-300',
 				)}
 			>
-				<button
-					onClick={() => setIsActive(!isActive)}
-					className={clsx(
-						'absolute w-4 h-4 rounded-full bg-black-200',
-						isActive ? 'top-[3px] right-[3px]' : 'top-1 right-1',
-					)}
-				>
-					<Image className="m-auto" src="/small-x.svg" alt="삭제" width={12} height={12} />
-				</button>
-				<div className="relative w-auto grow aspect-square">
-					<Image className="w-auto h-auto object-cover" src="/logo/icon-red.svg" alt="로고" fill />
-				</div>
-				<div className="flex gap-0.5">
-					<Image src="/draggable.svg" alt="왼쪽 화살표" width={18} height={18} />
-				</div>
+				{/* 팀 선택 취소 x 버튼 */}
+				{!(orderNum === 1 && !team) && (
+					// 1순위 팀 선택 전에는 x 버튼 표시 안 함
+					<button
+						onClick={onClickXButton}
+						className={clsx(
+							'absolute w-4 h-4 rounded-full bg-black-200',
+							isActive ? 'top-[3px] right-[3px]' : 'top-1 right-1',
+						)}
+					>
+						<Image className="m-auto" src="/small-x.svg" alt="삭제" width={12} height={12} />
+					</button>
+				)}
+
+				{/* 팀 로고 & draggable 아이콘 */}
+				{team && (
+					<>
+						<div className="relative w-auto grow aspect-square">
+							<Image className="w-auto h-auto object-cover" src="/logo/icon-red.svg" alt="로고" fill />
+						</div>
+						<div className="flex gap-0.5">
+							<Image src="/draggable.svg" alt="왼쪽 화살표" width={18} height={18} />
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
--- src/components/common/account/favorite-team-list.tsx
+++ src/components/common/account/favorite-team-list.tsx
@@ -6,24 +6,63 @@ import FavoriteTeamItem from './favorite-team-item';
 import Image from 'next/image';
 
 export default function FavoriteTeamList() {
-	const [teamList, setTeamList] = useState<TeamDto[]>([
-		{ pk: 1, nameKr: '', nameEn: '', logoUrl: '' },
-		{ pk: 1, nameKr: '', nameEn: '', logoUrl: '' },
+	const [teamList, setTeamList] = useState<(TeamDto | null)[]>([
+		{ pk: 1, logoUrl: '', nameEn: '', nameKr: '' },
+		{ pk: 2, logoUrl: '', nameEn: '', nameKr: '' },
+		{ pk: 3, logoUrl: '', nameEn: '', nameKr: '' },
 	]);
+	const [selectedTeamIndex, setSelectedTeamIndex] = useState(0);
+
+	// favorite team item 추가 버튼 클릭 핸들러
+	const handleAddButtonClick = () => {
+		setTeamList([...teamList, null]);
+		setSelectedTeamIndex(teamList.length);
+	};
+
+	// favorite team item 내부 x 버튼 클릭 핸들러
+	const handleXButtonClick = (e: React.MouseEvent, index: number) => {
+		e.stopPropagation();
+
+		// 모두 삭제 시 teamList를 [null]로 설정하여 추가 버튼이 생성되지 않도록 함
+		const newTeamList = teamList.filter((_, i) => i !== index);
+		setTeamList(newTeamList.length ? newTeamList : [null]);
+
+		// 현재 선택된 팀을 삭제할 경우 첫 번째 요소나 마지막 요소를 active
+		if (selectedTeamIndex === index) {
+			const newSelectedTeamIndex = index === 0 ? 0 : newTeamList.length - 1;
+			setSelectedTeamIndex(newSelectedTeamIndex);
+		} else {
+			// 그 외 기존 요소에 대한 active 유지
+			const selectedTeam = teamList[selectedTeamIndex];
+			const newSelectedTeamIndex = newTeamList.indexOf(selectedTeam);
+			setSelectedTeamIndex(newSelectedTeamIndex);
+		}
+	};
 
 	return (
 		<div className="grid grid-cols-3 gap-2.5 items-end">
 			{teamList.map((team, i) => (
-				<FavoriteTeamItem key={team?.pk} orderNum={i + 1} />
+				<FavoriteTeamItem
+					key={team?.pk ?? -1}
+					orderNum={i + 1}
+					team={team}
+					isActive={selectedTeamIndex === i}
+					onClickItem={() => setSelectedTeamIndex(i)}
+					onClickXButton={(e) => handleXButtonClick(e, i)}
+				/>
 			))}
-			<button
-				className="w-full h-auto aspect-[5/4] flex flex-col gap-1 justify-center items-center 
+			{teamList.at(-1) !== null && teamList.length < 3 && (
+				// 이전 팀 선택이 완료되고 선택 팀이 3개 미만일 때 추가 버튼 표시
+				<button
+					onClick={handleAddButtonClick}
+					className="w-full h-auto aspect-[5/4] flex flex-col gap-1 justify-center items-center 
           rounded-lg bg-black-000 p-[5px] border border-black-300"
-			>
-				<div className="relative w-12 h-12 @mobile:w-[2.1875rem] @mobile:h-[2.1875rem]">
-					<Image src={'/plus.svg'} alt="팀 추가 버튼" fill className="w-auto h-auto" />
-				</div>
-			</button>
+				>
+					<div className="relative w-12 h-12 @mobile:w-[2.1875rem] @mobile:h-[2.1875rem]">
+						<Image src={'/plus.svg'} alt="팀 추가 버튼" fill className="w-auto h-auto" />
+					</div>
+				</button>
+			)}
 		</div>
 	);
 }
