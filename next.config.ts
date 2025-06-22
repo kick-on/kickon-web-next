106235a06d6054716bff87dfea7509b3e9237c5a
지민재 <mimizae@jiminjaeui-MacBookPro.local>
2025-06-22 22:42:48 +0900

    feat: 모든 외부 https 도메인 허용, 카카오 http 도메인 허용 후 next/image 최적화 적용

--- next.config.ts
+++ next.config.ts
@@ -9,39 +9,10 @@ const nextConfig: NextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: 'k.kakaocdn.net',
-			},
-			{
-				protocol: 'https',
-				hostname: 'premierskillsenglish.britishcouncil.org',
-			},
-			{
-				protocol: 'https',
-				hostname: 'media.gettyimages.com',
-			},
-			{
-				protocol: 'https',
-				hostname: 'kickon-files-bucket.s3.ap-northeast-2.amazonaws.com',
-			},
-			{
-				protocol: 'https',
-				hostname: 'media.api-sports.io',
-			},
-			{
-				protocol: 'https',
-				hostname: 'i.ytimg.com',
-			},
-			{
-				protocol: 'https',
-				hostname: 'img1.kakaocdn.net',
-			},
-			{
-				protocol: 'https',
-				hostname: 'ssl.pstatic.net',
+				hostname: '**',
 			},
 		],
 		dangerouslyAllowSVG: true, // SVG 허용 (보안 주의)
-		unoptimized: true, // 모든 이미지 최적화 비활성화 → 외부 이미지 무제한 허용
 	},
 	webpack(config) {
 		config.module.rules.push({
