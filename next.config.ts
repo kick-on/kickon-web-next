aaabbf41d19fd5510beaebbe44f1d18ba07917ae
지민재 <mimizae@jiminjaeui-MacBookPro.local>
2025-06-23 01:32:11 +0900

    fix: 와일드 카드 제거해 특정 외부 도메인의 이미지만 허용

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,39 @@ const nextConfig: NextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: '**',
+				hostname: 'k.kakaocdn.net',
+			},
+			{
+				protocol: 'https',
+				hostname: 'premierskillsenglish.britishcouncil.org',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.gettyimages.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'kickon-files-bucket.s3.ap-northeast-2.amazonaws.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.api-sports.io',
+			},
+			{
+				protocol: 'https',
+				hostname: 'i.ytimg.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'img1.kakaocdn.net',
+			},
+			{
+				protocol: 'https',
+				hostname: 'ssl.pstatic.net',
+			},
+			{
+				protocol: 'https',
+				hostname: 'img.coupangstreaming.com',
 			},
 		],
 		dangerouslyAllowSVG: true, // SVG 허용 (보안 주의)
