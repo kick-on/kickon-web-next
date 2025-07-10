22a20edfb3cdda1f73073eafdf84601a8d851bc4
young <azerosong@gmail.com>
2025-07-10 18:36:06 +0900

    feat: 내비게이션바에 승부예측 버튼 추가

--- src/components/layouts/root/navbar/nav-button.tsx
+++ src/components/layouts/root/navbar/nav-button.tsx
@@ -1,33 +1,18 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { NavButton as NavButtonType } from '.';
 
-export interface NavButtonProps {
-	href: string;
-	content: string;
-}
-
-export default function NavButton({ href, content }: NavButtonProps) {
+export default function NavButton({ href, content, isActive }: NavButtonType) {
 	const pathname = usePathname();
-	const isActive = (() => {
-		if (content === '뉴스') return pathname.split('/').includes('news');
-		if (content === '클럽 커뮤니티') return pathname.split('/').includes('board');
-		if (content === '랭킹') return pathname.split('/').includes('ranking');
-	})();
 
 	return (
 		<Link
 			href={href}
 			className={clsx('w-[9.375rem] tablet:w-[146px] h-[4.5rem] flex items-center justify-center nav1-medium', {
 				'text-black-900': pathname === '/',
-				'text-black-000':
-					(pathname !== '/' && !(pathname.startsWith('/news') || pathname.startsWith('/board'))) || isActive,
-				'text-black-600':
-					(pathname.startsWith('/news') ||
-						pathname.startsWith('/board') ||
-						pathname.startsWith('/post') ||
-						pathname.startsWith('/ranking')) &&
-					!isActive,
+				'text-black-000': pathname !== '/' && isActive,
+				'text-black-600': !isActive,
 			})}
 		>
 			{content}
