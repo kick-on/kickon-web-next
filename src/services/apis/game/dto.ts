1000d59a11889c7ee25264903faefbf6e285de72
young <azerosong@gmail.com>
2025-07-14 19:12:12 +0900

    feat: 참여한 예측 리스트 조회 api 함수 생성

--- src/services/apis/game/dto.ts
+++ src/services/apis/game/dto.ts
@@ -1,5 +1,17 @@
 import { SuccessResponse } from '@/services/config/dto';
+import { GameTaggedLeagueDto } from '../user-game-gamble/dto';
 
+// 참여한 예측 리스트 조회
+export interface GetMyPredictionsRequest {
+	from: string; // YYYY-MM-DD
+	to: string; // YYYY-MM-DD
+}
+
+export type GetMyPredictionsResponse = SuccessResponse<MyPredictionsDto>;
+
+export type MyPredictionsDto = GameTaggedLeagueDto;
+
+// 예측 통계 조회
 export type GetMyStatsResponse = SuccessResponse<MyStatsDto>;
 
 export interface MyStatsDto {
--- src/services/apis/game/index.ts
+++ src/services/apis/game/index.ts
@@ -1,6 +1,28 @@
 import { fetcher } from '@/lib/server/fetcher';
 import { FailResponse } from '@/services/config/dto';
-import { GetMyStatsResponse } from './dto';
+import { GetMyPredictionsRequest, GetMyPredictionsResponse, GetMyStatsResponse } from './dto';
+
+// 내가 참여한 예측 리스트 조회
+export const getMyPredictions = async ({
+	from,
+	to,
+}: GetMyPredictionsRequest): Promise<GetMyPredictionsResponse | null> => {
+	const params = new URLSearchParams();
+
+	params.append('from', from);
+	params.append('to', to);
+
+	const response = await fetcher<GetMyPredictionsResponse | FailResponse>({
+		method: 'GET',
+		url: `/api/game/my-predictions?${params.toString()}`,
+	});
+
+	if (!response.code.split('_').includes('SUCCESS')) {
+		console.error('참여한 예측 리스트 조회 실패:', response);
+		return null;
+	}
+	return response;
+};
 
 // 예측 통계 조회
 export const getMyStats = async (): Promise<GetMyStatsResponse | null> => {
