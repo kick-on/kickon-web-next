0b40eeedd2b4bcf4a54ec741df00915604391af6
young <azerosong@gmail.com>
2025-07-14 21:09:05 +0900

    fix: TOP5 뉴스 레이아웃 변화를 적응형으로 수정

--- src/components/layouts/with-side/most-read-news-list/most-read-news-item.tsx
+++ src/components/layouts/with-side/most-read-news-list/most-read-news-item.tsx
@@ -1,15 +1,20 @@
+'use client';
+
+import useIsDesktop from '@/lib/hooks/useIsDesktop';
 import { HotNewsDto } from '@/services/apis/news/dto';
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export type MostReadNewsItemProps = Pick<HotNewsDto, 'pk' | 'title' | 'leagueNameKr' | 'thumbnailUrl'>;
 
 export default function MostReadNewsItem({ pk, title, leagueNameKr, thumbnailUrl }: MostReadNewsItemProps) {
+	const isDesktop = useIsDesktop();
+
 	return (
 		<Link
 			href={`/news/${pk}`}
-			className="grid grid-cols-[auto_1fr] gap-2 border-t border-black-200 p-4
-			@mobile:border-0 @mobile:p-0"
+			className={clsx('grid grid-cols-[auto_1fr] gap-2 border-black-200', isDesktop ? 'border-t p-4' : 'border-0 p-0')}
 		>
 			<Image
 				width={80}
