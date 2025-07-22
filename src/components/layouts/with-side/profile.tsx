ccfd785bf7191fc9712079cdd01a3fe7405fb5c1
young <azerosong@gmail.com>
2025-07-22 23:58:27 +0900

    fix: 프로필 팀 로고 중앙 정렬

--- src/components/layouts/with-side/profile.tsx
+++ src/components/layouts/with-side/profile.tsx
@@ -100,7 +100,7 @@ export default function Profile() {
 									/>
 								</div>
 								<div className="flex flex-col gap-[0.3125rem] mt-[0.4688rem]">
-									<div className="flex gap-2">
+									<div className="flex gap-2 items-center">
 										<div className="flex gap-1 h-fit items-center">
 											<div className="title5-semibold">{currentUserInfo?.nickname}</div>
 											<div className="body3-regular text-black-800">님</div>
