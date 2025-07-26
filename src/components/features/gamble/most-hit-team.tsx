4cec1e03581f629548879af55fd5cd508be2c60a
young <azerosong@gmail.com>
2025-07-26 10:55:14 +0900

    fix: class 동적 생성 문제 해결

--- src/components/features/gamble/most-hit-team.tsx
+++ src/components/features/gamble/most-hit-team.tsx
@@ -1,5 +1,3 @@
-import clsx from 'clsx';
-
 export default function MostHitTeam({
 	teamName,
 	teamLogo,
@@ -9,23 +7,20 @@ export default function MostHitTeam({
 	teamLogo: string;
 	teamColor: string;
 }) {
-	const teamColorClassName = `border-[${teamColor}] before:bg-[${teamColor}] after:bg-[${teamColor}]`;
-	const teamLogoClassName = teamLogo ? `bg-[url(${teamLogo})]` : 'bg-[url(/kick/red.svg)] ';
-
 	return (
 		<div
-			className={clsx(
-				`relative overflow-hidden h-15 body7-medium border rounded-lg bg-black-100
+			className="relative overflow-hidden h-15 body7-medium bg-black-100
+				border rounded-lg border-[var(--team-color)]
         before:content-[''] before:absolute before:top-1/2 before:-right-5
-        before:h-[13px] before:w-20 before:rotate-311 
+        before:h-[13px] before:w-20 before:rotate-311 before:bg-[var(--team-color)]
         after:content-[''] after:absolute after:top-1/2 after:-translate-y-1/2
-        after:-right-3 after:h-[13px] after:w-30 after:rotate-311`,
-				teamColorClassName,
-			)}
+        after:-right-3 after:h-[13px] after:w-30 after:rotate-311 after:bg-[var(--team-color)]"
+			style={{ '--team-color': teamColor } as React.CSSProperties}
 		>
 			<div
-				className={`absolute z-10 top-1/2 left-4 -translate-y-1/2 w-27 h-auto aspect-square
-          ${teamLogoClassName} bg-no-repeat bg-center bg-contain opacity-20`}
+				className="absolute z-10 top-1/2 left-4 -translate-y-1/2
+          bg-no-repeat bg-center bg-contain opacity-20 w-27 h-auto aspect-square"
+				style={{ backgroundImage: `url(${teamLogo || '/kick/red.svg'})` }}
 			/>
 			<div className="absolute z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-5/11 w-[60%] text-center">
 				<span className="font-semibold break-keep">{teamName}</span>의 승부예측을 가장 많이 적중했어요.
