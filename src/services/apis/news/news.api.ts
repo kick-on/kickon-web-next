fcb5ed8a14921aa90a6c3a4afe1635d3ca3004c6
young <azerosong@gmail.com>
2025-07-26 12:12:21 +0900

    refactor: 뉴스 리스트 조회 api 분리

--- src/services/apis/news/news.api.ts
+++ src/services/apis/news/news.api.ts
@@ -1,6 +1,7 @@
 import { fetcher } from '@/lib/server/fetcher';
-import { GetDetailResponse, GetHotNewsResponse } from './news.type';
+import { GetDetailResponse, GetHotNewsResponse, GetNewsListRequest, GetNewsListResponse } from './news.type';
 import { SERVER_URL } from '@/services/config/constants';
+import { FailResponse } from '@/services/config/dto';
 
 // 뉴스 상세 조회
 export const getNewsDetailContent = async (id: number): Promise<GetDetailResponse | null> => {
@@ -16,6 +17,47 @@ export const getNewsDetailContent = async (id: number): Promise<GetDetailRespons
 	} catch (error) {
 		console.error('상세페이지 조회 실패:', error);
 		throw error;
+	}
+};
+
+// 뉴스 리스트 조회
+export const getNewsList = async ({
+	team,
+	size,
+	page,
+	order,
+	league,
+	infinite,
+	lastNews,
+	lastViewCount,
+}: GetNewsListRequest) => {
+	const params = new URLSearchParams();
+
+	params.append('order', String(order));
+	params.append('size', String(size));
+	params.append('page', String(page));
+
+	if (team !== undefined) params.append('team', String(team));
+	if (league !== undefined) params.append('league', String(league));
+	// 무한 스크롤
+	if (infinite !== undefined) params.append('infinite', String(infinite));
+	if (lastNews !== undefined) params.append('lastNews', String(lastNews));
+	if (lastViewCount !== undefined) params.append('lastViewCount', String(lastViewCount));
+
+	try {
+		const response = await fetcher<GetNewsListResponse | FailResponse>({
+			method: 'GET',
+			url: `/api/news?${params.toString()}`,
+		});
+
+		if (!response.code.split('_').includes('SUCCESS')) {
+			console.error('뉴스 리스트 조회 실패: ', `${SERVER_URL}/api/news?${params.toString()}`, response);
+			return null;
+		}
+
+		return response;
+	} catch (error) {
+		console.error('뉴스 리스트 조회 실패: ', `${SERVER_URL}/api/news?${params.toString()}`, error);
 	}
 };
 
