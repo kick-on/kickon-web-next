ee8cc9355a77d264aaa3c7b671d4818b2fc1efcf
young <azerosong@gmail.com>
2025-07-26 14:25:07 +0900

    refactor: 불필요한 파일 삭제

--- src/lib/utils/getAccessToken.ts
+++
@@ -1,8 +0,0 @@
-export const getAccessToken = (): string | null => {
-	if (typeof window === 'undefined') return null; // SSR 방지
-	return localStorage.getItem('accessToken');
-};
-export const getRefreshToken = (): string | null => {
-	if (typeof window === 'undefined') return null; // SSR 방지
-	return localStorage.getItem('refreshToken');
-};
--- src/services/apis/detail/actions/dto.ts
+++
@@ -1,4 +0,0 @@
-import { PostContentsRequest } from '../../post/dto';
-
-// 수정하기 요청
-export type PatchContentsRequest = Partial<PostContentsRequest>;
