840aeee4599b300cb036bbd4450beba73968368e
young <azerosong@gmail.com>
2025-07-29 19:56:35 +0900

    feat: 하프타임 동영상 페이지에 동영상 모조데이터 추가

--- src/app/halftime/[id]/page.tsx
+++ src/app/halftime/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Player from '@/components/features/halftime/player';
 import { Keyboard, Mousewheel } from 'swiper/modules';
 import { SwiperSlide, Swiper } from 'swiper/react';
 
@@ -30,7 +31,19 @@ export default function Page() {
 					<SwiperSlide
 						key={i}
 						className="bg-black-300 w-auto h-full aspect-[2/3] @mobile:h-auto @mobile:w-full rounded-lg overflow-hidden"
-					></SwiperSlide>
+					>
+						{({ isActive }) =>
+							i === 1 ? (
+								<Player index={i} autoplay={isActive} src="/video/test1.mp4" />
+							) : i === 2 ? (
+								<Player index={i} autoplay={isActive} src="/video/test2.mp4" />
+							) : i === 3 ? (
+								<Player index={i} autoplay={isActive} src="https://www.youtube.com/watch?v=-NMmHBIijKg" />
+							) : (
+								<Player index={i} autoplay={isActive} src="https://www.youtube.com/shorts/KrMk5Ew-Vus" />
+							)
+						}
+					</SwiperSlide>
 				))}
 			</Swiper>
 		</div>
