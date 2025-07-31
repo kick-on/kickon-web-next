334759d65efff1e9a4fa38161f86201c3fc518fd
young <azerosong@gmail.com>
2025-08-01 00:29:29 +0900

    feat: 액션 버튼에 아이콘 추가

--- src/components/features/halftime/player.tsx
+++ src/components/features/halftime/player.tsx
@@ -1,13 +1,36 @@
 'use client';
 
-import useIsDesktop from '@/lib/hooks/useIsDesktop';
 import clsx from 'clsx';
+import Image from 'next/image';
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
 
+const actionButtons = [
+	{
+		label: '킥',
+		value: '',
+		src: '/kick/fill-none.svg',
+	},
+	// {
+	// 	label: '댓글',
+	//	value:'',
+	// 	src: '/comment.svg',
+	// },
+	{
+		label: '공유',
+		value: '공유',
+		src: '/share.svg',
+	},
+	{
+		label: '본문',
+		value: '본문',
+		src: '/paper.svg',
+	},
+];
+
 export default function Player({ src, index, autoplay }: { src: string; index: number } & Partial<HTMLMediaElement>) {
 	const [isPlaying, setIsPlaying] = useState(true);
-	const isDesktop = useIsDesktop();
+	const [isMuted, setIsMuted] = useState(true);
 
 	return (
 		<div
@@ -16,20 +39,29 @@ export default function Player({ src, index, autoplay }: { src: string; index: n
 			onClick={() => setIsPlaying(!isPlaying)}
 		>
 			<div className="absolute z-15 top-4 left-4 right-4 flex justify-between items-center">
-				<button className="w-9 h-9 bg-black-900/10 rounded-full"></button>
-				<button className="w-9 h-9 bg-black-900/10 rounded-full"></button>
+				<button className="w-9 h-9 p-1.5 bg-black-900/10 rounded-full">
+					<Image src={'/chevron/right-white.svg'} alt="뒤로가기" width={24} height={24} className="rotate-180" />
+				</button>
+				<button className="w-9 h-9 p-1.5 bg-black-900/10 rounded-full" onClick={() => setIsMuted(!isMuted)}>
+					<Image src={isMuted ? '/mute.svg' : '/volume.svg'} alt={isMuted ? '소리' : '음소거'} width={24} height={24} />
+				</button>
 			</div>
 
 			<div
-				className={clsx(
-					'absolute z-15 py-6 px-4 flex flex-col gap-8 rounded-lg shadow-calendar',
-					isDesktop
-						? 'bg-black-000/20 border border-black-200 bottom-24 -right-20'
-						: 'bg-black-900/10 bottom-8 tablet:right-4 @mobile:right-3',
-				)}
+				className="absolute z-15 py-6 px-3 flex flex-col gap-8 rounded-lg shadow-calendar
+					desktop:border desktop:border-black-200
+					desktop:bg-black-000/20 desktop:bottom-24 desktop:-right-20
+					bg-black-900/10 bottom-8 tablet:right-4 @mobile:right-3"
 			>
-				{[1, 2, 3, 4].map((i) => (
-					<div key={i} className="w-[1.625rem] h-[2.875rem]"></div>
+				{actionButtons.map((button) => (
+					<button
+						key={button.src}
+						className="px-2 flex flex-col gap-1.5 items-center body7-medium text-black-600
+							tablet:brightness-0 tablet:invert @mobile:brightness-0 @mobile:invert"
+					>
+						<Image src={button.src} alt={button.label} width={24} height={24} />
+						<span>{button.value || '1.2천'}</span>
+					</button>
 				))}
 			</div>
 
