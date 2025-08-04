a9d2f0d02e2b135c869b89c59f40c28d2883dc97
mimizae <mimizae@jiminjaeui-MacBookPro.local>
2025-08-04 21:25:22 +0900

    refactor: 내브바 right-button 공통 컴포넌트 생성

---
+++ src/components/layouts/root/navbar/right-button.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Suspense } from 'react';
+import { usePathname } from 'next/navigation';
+import NoticeButton from './notice-button';
+import LoginButton from './login-button';
+import clsx from 'clsx';
+
+interface RightButtonsProps {
+	isMobile?: boolean;
+	isTabletWidth?: boolean;
+	onClickProfile?: () => void;
+}
+
+export default function RightButtons({ isMobile = false, isTabletWidth = false, onClickProfile }: RightButtonsProps) {
+	const pathname = usePathname();
+
+	return (
+		<div className={clsx('flex items-center justify-center', isMobile ? ' gap-[18px] ml-auto' : 'gap-6')}>
+			<NoticeButton />
+			<Suspense>
+				{isMobile ? (
+					<LoginButton onClickProfile={onClickProfile} />
+				) : (
+					(pathname === '/signup' || isTabletWidth) && <LoginButton />
+				)}
+			</Suspense>
+		</div>
+	);
+}
