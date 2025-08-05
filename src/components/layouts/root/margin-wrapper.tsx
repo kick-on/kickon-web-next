496c285b7adbc2ef83c49e785fcaed5b1a716f79
young <azerosong@gmail.com>
2025-08-05 15:52:38 +0900

    feat: 루트 레이아웃에 padding wrapper 추가

--- src/components/layouts/root/margin-wrapper.tsx
+++ src/components/layouts/root/margin-wrapper.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import useIsDesktop from '@/lib/hooks/useIsDesktop';
+import useIsLeftSideVisible from '@/lib/hooks/useIsLeftSideVisible';
 import { isFullScreen } from '@/lib/utils/isFullScreen';
 import { usePathname } from 'next/navigation';
 
 export default function MarginWrapper({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
-	const isDesktop = useIsDesktop();
+	const isMobileNavbar = !useIsLeftSideVisible();
 
-	return <div className={isDesktop || isFullScreen(pathname) ? '' : 'mt-16'}>{children}</div>;
+	return <div className={isMobileNavbar && !isFullScreen(pathname) ? 'mt-16' : ''}>{children}</div>;
 }
