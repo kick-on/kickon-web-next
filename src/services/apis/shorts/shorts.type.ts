47e66b6493ca0e6a463b1e56261eddac0f874d8a
young <azerosong@gmail.com>
2025-08-07 18:08:57 +0900

    fix: 서버 응답 수정에 따라 get todays halftime 객체 수정

--- src/services/apis/shorts/shorts.type.ts
+++ src/services/apis/shorts/shorts.type.ts
@@ -2,11 +2,13 @@ import { SuccessResponse } from '@/services/config/dto';
 
 // 오늘의 하프타임 조회
 export interface GetTodaysHalftimeDto {
+	pk: number;
 	s3Key: string;
 	usedIn: 'BOARD' | 'NEWS';
-	referencePk: number;
+	referencePk: number; // 원본 글 pk
 	title: string;
 	viewCount: number;
 	kickCount: number;
+	createdAt: string;
 }
 export type GetTodaysHalftimeResponse = SuccessResponse<GetTodaysHalftimeDto[]>;
