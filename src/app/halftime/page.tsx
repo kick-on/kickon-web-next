ddf864b6d57e1c8a323c176041d5b01c268356b1
young <azerosong@gmail.com>
2025-08-09 16:45:52 +0900

    fix: 하프타임 리스트 페이지 pt 대신 pt 적용

--- src/app/halftime/page.tsx
+++ src/app/halftime/page.tsx
@@ -11,38 +11,40 @@ const data = {
 
 export default function Page() {
 	return (
-		<ComponentFrame
-			className="@mobile:w-full! max-[1440px]:w-[80%]! w-[90%]! max-w-[1360px]
-				mx-auto mt-4 @mobile:mt-0 @mobile:bg-transparent @mobile:border-0
-				px-[7.125rem] max-[1440px]:px-4 space-y-6 py-6"
-		>
-			<Suspense>
-				<Sorter />
-			</Suspense>
+		<div className="pt-4 @mobile:pt-0">
+			<ComponentFrame
+				className="@mobile:w-full! max-[1440px]:w-[80%]! w-[90%]! max-w-[1360px]
+			mx-auto @mobile:bg-transparent @mobile:border-0
+			px-[7.125rem] max-[1440px]:px-4 space-y-6 py-6"
+			>
+				<Suspense>
+					<Sorter />
+				</Suspense>
 
-			<div
-				className="grid grid-cols-5 gap-6
-						max-[1440px]:grid-cols-4 max-[1440px]:gap-x-3
+				<div
+					className="grid grid-cols-5 gap-6
+				max-[1440px]:grid-cols-4 max-[1440px]:gap-x-3
 						max-[1094px]:grid-cols-3 max-[1094px]:gap-x-3
 						@mobile:grid-cols-2 @mobile:gap-4"
-			>
-				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 22, 33, 44, 55, 66, 77].map((i) => (
-					<PreviewWithTitle
-						key={i}
-						pk={i}
-						title={data.title}
-						viewCount={data.viewCount}
-						kickCount={data.kickCount}
-						videoUrl={
-							i % 3 === 0
-								? '/video/test1.mp4'
-								: i % 3 === 1
-									? 'https://www.youtube.com/shorts/R39fXsyQN_o'
-									: 'https://youtu.be/3-G0Z_sjRiQ?si=JN6gcWPQ5Sp3ffw6'
-						}
-					/>
-				))}
-			</div>
-		</ComponentFrame>
+				>
+					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 22, 33, 44, 55, 66, 77].map((i) => (
+						<PreviewWithTitle
+							key={i}
+							pk={i}
+							title={data.title}
+							viewCount={data.viewCount}
+							kickCount={data.kickCount}
+							videoUrl={
+								i % 3 === 0
+									? '/video/test1.mp4'
+									: i % 3 === 1
+										? 'https://www.youtube.com/shorts/R39fXsyQN_o'
+										: 'https://youtu.be/3-G0Z_sjRiQ?si=JN6gcWPQ5Sp3ffw6'
+							}
+						/>
+					))}
+				</div>
+			</ComponentFrame>
+		</div>
 	);
 }
