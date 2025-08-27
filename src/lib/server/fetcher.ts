a7f699dc213a27910b7e352011736476e622f75a
young <azerosong@gmail.com>
2025-08-27 16:58:31 +0900

    refactor: fetcher 함수에서 proxy 대신 바로 백엔드 api 콜하도록 수정

--- src/lib/server/fetcher.ts
+++ src/lib/server/fetcher.ts
@@ -1,12 +1,19 @@
 import { ProxyParameter } from '@/app/api/proxy/route';
-import { DOMAIN_URL } from '@/services/config/constants';
+import { SERVER_URL } from '@/services/config/constants';
+import useAuthStore from '../store/useAuthStore';
 
-// proxy 경로로 라우팅하는 함수
-export async function fetcher<T>(body: ProxyParameter, revalidate?: number) {
-	const response = await fetch(`${DOMAIN_URL}/api/proxy`, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify(body),
+export async function fetcher<T>({ url, method, headers, body }: ProxyParameter, revalidate?: number) {
+	const { accessToken } = useAuthStore.getState();
+	const hasBody = !!body;
+
+	const response = await fetch(`${SERVER_URL}${url}`, {
+		method,
+		headers: {
+			...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+			...(hasBody ? { 'Content-Type': 'application/json' } : {}),
+			...headers,
+		},
+		body: hasBody ? JSON.stringify(body) : undefined,
 		next: { revalidate: revalidate ?? 0 }, // revalidate를 안 넘기면 캐싱 안 함
 	});
 
