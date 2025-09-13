f057fc58e849756975e81d63825bc6f6ec3bdb0c
송아영 <gustn@song-ayeong-ui-MacBookPro.local>
2025-09-13 13:11:51 +0900

    chore: pm2에서 modules 설치 및 빌드 실행하도록 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e  # 에러 발생 시 즉시 종료
 
 # 프로젝트 디렉토리로 이동
 cd /home/ubuntu/web-nextjs-service
@@ -7,7 +8,15 @@ cd /home/ubuntu/web-nextjs-service
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 
-# PM2 재시작
+# 기존 node_modules 삭제 후 lockfile 기반 설치
+rm -rf node_modules
+yarn install --frozen-lockfile
+
+# 기존 .next 삭제 후 재빌드
+rm -rf .next
+yarn build
+
+# pm2 재시작
 pm2 delete my-front-app --silent || true
 pm2 start yarn --name "my-front-app" -- start
 pm2 save
