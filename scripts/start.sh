6428d753130055abc4576828889554e0accfd890
gustn99 <azerosong@gmail.com>
2025-12-25 18:10:31 +0900

    hotfix: github action에서 환경변수 주입

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 인자로 환경 선택 (default: production)
-ENV=${1:-production}
-
 # 프로젝트 디렉토리로 이동
 cd /home/ubuntu/web-nextjs-service
 
@@ -10,20 +7,17 @@ cd /home/ubuntu/web-nextjs-service
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# 앱 이름과 포트 결정
+# ENV 환경 변수 확인 (AWS CLI 배포에서 전달됨)
+ENV=${ENV:-production}  # ENV가 없으면 기본 production
+
+# ENV에 따라 pm2 앱 이름 결정
 if [ "$ENV" = "development" ]; then
-    APP_NAME="my-front-app-dev"
-    PORT=3001
+  APP_NAME="my-front-app-dev"
 else
-    APP_NAME="my-front-app-prod"
-    PORT=3000
+  APP_NAME="my-front-app-prod"
 fi
 
-# 기존 pm2 프로세스 삭제
+# PM2 재시작
 pm2 delete "$APP_NAME" --silent || true
-
-# pm2 실행
-pm2 start node_modules/next/dist/bin/next --name "$APP_NAME" -- start -p $PORT
-
-# pm2 상태 저장
+pm2 start ecosystem.config.cjs --only "$APP_NAME"
 pm2 save
