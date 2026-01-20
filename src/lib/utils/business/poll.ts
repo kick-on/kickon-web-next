24369869db1f28313deb9f1c3cf0b519506fc7b2
gustn99 <azerosong@gmail.com>
2026-01-20 17:18:49 +0900

    refactor: 투표 컴포넌트 내 게시글 pk 조회 로직 개선

--- src/lib/utils/business/poll.ts
+++ src/lib/utils/business/poll.ts
@@ -1,9 +1,9 @@
 export const getEditingBoardPk = () => {
-		const detailData = sessionStorage.getItem('detailContent');
+	const detailData = sessionStorage.getItem('detailContent');
 
-		if (detailData) {
-			const parsedData = JSON.parse(detailData);
-			return parsedData.data.pk;
-		}
-		return null;
+	if (detailData) {
+		const parsedData = JSON.parse(detailData);
+		return parsedData.data.pk;
+	}
+	return null;
 };
