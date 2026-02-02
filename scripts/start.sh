3b917b1a3e1a4183cbabc1a43bd9af9ce4b4d796
gustn99 <azerosong@gmail.com>
2026-02-02 15:49:32 +0900

    hotfix: dev/prod 프로젝트 디렉토리 분기 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# 프로젝트 디렉토리 분기
+BASE_PATH="/home/ubuntu"
+
+if [[ "$APPLICATION_NAME" == *"prod"* ]]; then
+    TARGET_DIR="web-nextjs-prod-service"
+else
+    TARGET_DIR="web-nextjs-dev-service"
+fi
+
 # 프로젝트 디렉토리로 이동
-cd /home/ubuntu/web-nextjs-service
+cd "$BASE_PATH/$TARGET_DIR"
 
 # nvm 환경 설정
 export NVM_DIR="$HOME/.nvm"
