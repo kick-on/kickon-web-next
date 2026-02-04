ca20b3c222a2cedb76926bdfc06b86f2cde16e89
gustn99 <azerosong@gmail.com>
2026-02-04 15:31:54 +0900

    style: 주석 처리된 기능 제거

--- src/app/(with-side)/page.tsx
+++ src/app/(with-side)/page.tsx
@@ -4,9 +4,6 @@ import PredictLeagueTab from '@/components/features/home/predict-league-tab';
 import { useEffect } from 'react';
 
 export default function Home() {
-	// const { currentUserInfo } = useCurrentUserInfoStore();
-	// const isDesktop = useIsDesktop();
-
 	useEffect(() => {
 		document.body.style.backgroundColor = 'var(--color-black-800)';
 
@@ -30,17 +27,6 @@ export default function Home() {
 			<div className="bg-black-000 rounded-[0.625rem]">
 				<PredictLeagueTab />
 			</div>
-
-			{/*{!isDesktop && <TodaysHalftime />}*/}
-
-			{/* 추천 뉴스 및 게시글 */}
-			{/*<Suspense>*/}
-			{/*	<RecommendedContent*/}
-			{/*		mode={'news'}*/}
-			{/*		teamName={currentUserInfo?.favoriteTeams[0]?.nameKr || currentUserInfo?.favoriteTeams[0]?.nameEn || undefined}*/}
-			{/*	/>*/}
-			{/*	<RecommendedContent mode={'board'} />*/}
-			{/*</Suspense>*/}
 		</div>
 	);
 }
