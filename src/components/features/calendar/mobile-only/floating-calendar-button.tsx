cf9f66a539634ebb7fd6bccb27baf2d788f2c7cd
mimizae <jim400934@gmail.com>
2026-02-12 15:41:27 +0900

    feat: 플로팅 캘린터 버튼 구현

---
+++ src/components/features/calendar/mobile-only/floating-calendar-button.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+const FloatingCalendarButton = () => {
+	return (
+		<div
+			className="lg:hidden w-fit h-fit z-30 flex items-center sticky
+				bottom-15 ml-auto -mr-[5.125rem] max-[848px]:mr-4"
+		>
+			<button
+				onClick={() => {}}
+				className="w-[3.625rem] h-[3.625rem] bg-white rounded-full @mobile:mr-[1px]
+				transition-transform duration-200 ease-out
+				shadow-[0_2px_5px_rgba(0,0,0,0.12),0_2px_5px_rgba(0,0,0,0.24)]
+				active:scale-95"
+			>
+				<div className="flex items-center gap-2 px-[15px] w-full">
+					<Image src="/calendar.png" width={28} height={28} alt="" />
+				</div>
+			</button>
+		</div>
+	);
+};
+
+export default FloatingCalendarButton;
