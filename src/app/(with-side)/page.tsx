649df3f98192bfb7da1ff614c79f66e57aa98600
mimizae <jim400934@gmail.com>
2026-02-12 15:48:36 +0900

    chore: 태블릿 및 모바일에서의 캘린더 컴포넌트 제거

--- src/app/(with-side)/page.tsx
+++ src/app/(with-side)/page.tsx
@@ -1,17 +1,10 @@
 'use client';
 
-import MatchPredictionCalendar from '@/components/common/match-prediction-calendar';
 import PredictLeagueTab from '@/components/features/home/predict-league-tab';
-import { useNextMatchDateQuery } from '@/lib/hooks/queries/useNextMatchDateQuery';
-import useIsDesktop from '@/lib/hooks/useIsDesktop';
-import useIsTablet from '@/lib/hooks/useIsTablet';
-import clsx from 'clsx';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
 	// const { currentUserInfo } = useCurrentUserInfoStore();
-	const isDesktop = useIsDesktop();
-	const isTablet = useIsTablet();
 
 	useEffect(() => {
 		document.body.style.backgroundColor = 'var(--color-black-800)';
@@ -21,24 +14,8 @@ export default function Home() {
 		};
 	}, []);
 
-	const [selectedDate, setSelectedDate] = useState(new Date());
-
-	const { data: nextDate } = useNextMatchDateQuery();
-
-	useEffect(() => {
-		if (!nextDate) return;
-
-		const [year, month, date] = nextDate.split('-').map(Number);
-		setSelectedDate(new Date(year, month - 1, date));
-	}, [nextDate]);
-
 	return (
 		<div className="grid grid-cols-1 min-[120rem]:grid-cols-2 gap-6 pb-90">
-			{!isDesktop && (
-				<div className={clsx('bg-black-000 rounded-[0.625rem]', isTablet && 'max-w-[39.75rem] w-full mx-auto')}>
-					<MatchPredictionCalendar type="match" selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
-				</div>
-			)}
 			{/* 승부 예측 */}
 			<div className="bg-black-000 rounded-[0.625rem]">
 				<PredictLeagueTab />
